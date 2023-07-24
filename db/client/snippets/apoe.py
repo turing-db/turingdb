86244b624131d1104ed5a5e69cb79125247a0aba
luclabarriere <luc.labarriere@turing.bio>
2023-07-24 04:21:28 +0000

    [FIX] Corrected bug with parsing of edges during jsonNeo4j import

--- db/client/snippets/apoe.py
+++ db/client/snippets/apoe.py
@@ -4,8 +4,14 @@ from turingdb import Turing
 if __name__ == '__main__':
     turing = Turing("localhost:6666")
     db = turing.get_db("reactome")
+    apoe4_node_id = 2104313
 
-    apoe4_node = db.list_nodes_by_id([597221])[0]
+    # Find by id
+    apoe4_node = db.list_nodes(ids=[apoe4_node_id])[0]
+    # Find by property
+    # from turingdb import Property
+    # apoe4_node = db.list_nodes(property=Property("displayName", "APOE-4"))
+
     print(f"Found node: {apoe4_node.list_properties()['displayName']}")
 
     print("====IN NODES")
