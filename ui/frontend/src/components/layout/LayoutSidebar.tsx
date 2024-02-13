9149f1071b2dc7c90d5cfd4776c44d634490409c
Remy Boutonnet <remy.boutonnet@turing.bio>
2024-02-13 19:56:26 +0000

    UI: commenting unused side bar buttons and profile link

--- ui/frontend/src/components/layout/LayoutSidebar.tsx
+++ ui/frontend/src/components/layout/LayoutSidebar.tsx
@@ -12,6 +12,7 @@ import IconFolder from '@/assets/icons/folder.svg'
 import IconHelpCircle from '@/assets/icons/help-circle.svg'
 
 const navItems = [
+  /*
   {
     name: 'Dashboard',
     href: '/dashboard',
@@ -22,11 +23,13 @@ const navItems = [
     href: '/data',
     icon: <IconDatabase />,
   },
+*/
   {
     name: 'Search',
     href: '/',
     icon: <IconSearch />,
   },
+  /*
   {
     name: 'Flow',
     href: '/flow',
@@ -42,6 +45,7 @@ const navItems = [
     href: '/folder',
     icon: <IconFolder />,
   },
+*/
 ]
 
 const footerLinks = [
@@ -50,13 +54,16 @@ const footerLinks = [
     href: '/help',
     icon: <IconHelpCircle />,
   },
+  /*
   {
     name: 'Profile',
     href: '/profile',
     icon: 'user-circle',
   },
+*/
 ]
 
+/*
 export default function LayoutSidebar() {
   return (
     <nav className=" flex w-16 flex-shrink-0 flex-col justify-between gap-8 bg-grey-800 px-3.5 py-4">
@@ -78,6 +85,31 @@ export default function LayoutSidebar() {
         </li>
         <li>
           <LayoutSidebarItem name="Profile" href="/profile" img="/img/profile.png" />
+        </li>
+      </ul>
+    </nav>
+  )
+}
+*/
+
+export default function LayoutSidebar() {
+  return (
+    <nav className=" flex w-16 flex-shrink-0 flex-col justify-between gap-8 bg-grey-800 px-3.5 py-4">
+      <div className="flex flex-col gap-6">
+        <Link href="/">
+          <Image src="/logo.svg" width="36" height="36" alt="Turing" priority />
+        </Link>
+        <ul className="flex flex-col gap-2">
+          {navItems.map(({ name, href, icon }) => (
+            <li key={name}>
+              <LayoutSidebarItem name={name} href={href} icon={icon} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <ul className="flex flex-col gap-2">
+        <li>
+          <LayoutSidebarItem name="Help" href="/help" icon={<IconHelpCircle />} />
         </li>
       </ul>
     </nav>
