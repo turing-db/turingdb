408513540d38d90b788f19a075d5cffaf0146861
Luc Labarrière <luc.labarriere@turing.bio>
2024-03-04 09:10:28 +0000

    Added labelsets for nodes and a ScanNodesByLabel iterator

--- test/db2/core/LabelSetTest.cpp
+++ test/db2/core/LabelSetTest.cpp
@@ -21,4 +21,71 @@ TEST(LabelSetTest, LabelSet) {
     ASSERT_TRUE(set.hasAtLeastLabels({0, 50}));
     ASSERT_TRUE(set.hasAtLeastLabels({0, 5, 50}));
     ASSERT_FALSE(set.hasAtLeastLabels({0, 1}));
+
+    srand(0);
+    constexpr size_t count = 500;
+    using IntegerType = uint64_t;
+    constexpr size_t IntegerCount = 5;
+
+    std::vector<TemplateLabelSet<IntegerType, IntegerCount>> bitsets(count);
+    std::vector<IntegerType> uints(count);
+
+    for (size_t i = 0; i < count; i++) {
+        auto& bitset = bitsets[i];
+        auto& uint = uints[i];
+        size_t bitCount = rand() % TemplateLabelSet<IntegerType, IntegerCount>::bitCount();
+        for (size_t j = 0; j < bitCount; j++) {
+            size_t index = rand() % TemplateLabelSet<IntegerType, IntegerCount>::bitCount();
+            bitset.set(index);
+
+            uint |= (IntegerType)1 << index;
+        }
+    }
+
+    using namespace std;
+    using namespace chrono;
+
+    std::cout << "Sorting" << std::endl;
+    // Sorting
+    {
+        auto t0 = std::chrono::high_resolution_clock::now();
+        std::sort(bitsets.begin(), bitsets.end());
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto dur = duration_cast<microseconds>(t1 - t0).count();
+        std::cout << "Bitsets: " << dur / 1000.0f << " ms" << std::endl;
+    }
+
+    {
+        auto t0 = std::chrono::high_resolution_clock::now();
+        std::sort(uints.begin(), uints.end());
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto dur = duration_cast<microseconds>(t1 - t0).count();
+        std::cout << "Uints: " << dur / 1000.0f << " ms" << std::endl;
+    }
+
+    std::cout << "Hashing" << std::endl;
+    // Hashing
+    {
+        std::vector<size_t> hashes(bitsets.size());
+        auto t0 = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < hashes.size(); i++) {
+            hashes[i] = std::hash<TemplateLabelSet<IntegerType, IntegerCount>> {}(bitsets[i]);
+        }
+        auto t1 = std::chrono::high_resolution_clock::now();
+        std::sort(hashes.begin(), hashes.end());
+        auto dur = duration_cast<microseconds>(t1 - t0).count();
+        std::cout << "Bitsets: " << dur / 1000.0f << " ms" << std::endl;
+    }
+
+    {
+        std::vector<size_t> hashes(uints.size());
+        auto t0 = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < hashes.size(); i++) {
+            hashes[i] = std::hash<IntegerType> {}(uints[i]);
+        }
+        auto t1 = std::chrono::high_resolution_clock::now();
+        std::sort(hashes.begin(), hashes.end());
+        auto dur = duration_cast<microseconds>(t1 - t0).count();
+        std::cout << "Integers: " << dur / 1000.0f << " ms" << std::endl;
+    }
 }
