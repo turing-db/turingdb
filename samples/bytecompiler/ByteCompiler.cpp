67ca2dc91267aaa90625677e2f6a69c1c070d49a
remy <remy.boutonnet@turing.bio>
2024-06-24 19:45:54 +0000

    First implementation of BytecodeCompiler for path patterns

--- samples/bytecompiler/ByteCompiler.cpp
+++ samples/bytecompiler/ByteCompiler.cpp
@@ -4,7 +4,7 @@
 
 #include <spdlog/spdlog.h>
 
-#include "BytecodeGenerator.h"
+#include "BytecodeCompiler.h"
 #include "ASTContext.h"
 #include "QueryParser.h"
 
@@ -12,9 +12,21 @@ using namespace db;
 
 namespace {
 
+/*
 QueryCommand* selectAPOE(ASTContext* ctxt) {
     QueryParser parser(ctxt);
     const std::string queryStr = "SELECT * FROM protein[APOE4]";
+
+    QueryCommand* query = parser.parse(queryStr);
+    assert(query);
+
+    return query;
+}
+*/
+
+QueryCommand* selectGenesAndProteins(ASTContext* ctxt) {
+    QueryParser parser(ctxt);
+    const std::string queryStr = "SELECT p,g FROM (p:protein)--(g:gene)";
 
     QueryCommand* query = parser.parse(queryStr);
     assert(query);
@@ -29,7 +41,7 @@ int main(int argc, char** argv) {
     ASTContext ctxt;
 
     // Code generation
-    BytecodeGenerator byteGen(selectAPOE(&ctxt));
+    BytecodeCompiler byteGen(selectGenesAndProteins(&ctxt));
 
     const bool res = byteGen.generate();
     if (!res) {
