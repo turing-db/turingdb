92476274434bacfb141ea6d50c191c5f27bf13c3
cyrusknopf <cyrus.knopf@turing.bio>
2025-07-03 09:06:13 +0000

    Streamline setup (!67)

--- samples/prefix-tree/getdict.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-wget -P $ACTIVE_REPO/build/build_package/samples/prefix-tree/data http://www.cs.duke.edu/~ola/ap/linuxwords
--- samples/prefix-tree/setup.sh
+++ samples/prefix-tree/setup.sh
@@ -3,8 +3,5 @@
 ln -s $ACTIVE_REPO/src/samples/prefix-tree/sample.sh $ACTIVE_REPO/build/build_package/samples/prefix-tree/sample.sh
 chmod +x !!:3
 
-ln -s $ACTIVE_REPO/src/samples/prefix-tree/getdict.sh $ACTIVE_REPO/build/build_package/samples/prefix-tree/getdict.sh
-chmod +x !!:3
-
 # Get the linux words file into build
 wget -P $ACTIVE_REPO/build/build_package/samples/prefix-tree/data/ http://www.cs.duke.edu/~ola/ap/linuxwords
