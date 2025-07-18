7e83a130e9e5fa08a4bcd7cf363dc0d04e45dd3a
luclabarriere <luc.labarriere@turing.bio>
2025-07-18 11:33:32 +0000

    Added a parser2 sample and reverted plan2 sample (!81)

---
+++ samples/parser2/main.cpp
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+#include "CypherParser.h"
+#include "Time.h"
+#include "ParserException.h"
+#include "FileReader.h"
+
+using namespace db;
+
+void runParser2(const std::string& query);
+
+int main(int argc, char** argv) {
+    std::string queryStr;
+
+    if (argc > 1 && strlen(argv[1]) > 0) {
+        queryStr = argv[1];
+
+    } else {
+        fs::Path path(SAMPLE_DIR "/parser-queries.txt");
+        fmt::print("Reading query from file: {}\n", path.get());
+        fs::File file = fs::File::open(path).value();
+        fs::FileReader reader;
+
+        reader.setFile(&file);
+        reader.read();
+
+        auto it = reader.iterateBuffer();
+
+        queryStr = it.get<char>(file.getInfo()._size);
+    }
+
+    runParser2(queryStr);
+
+    return EXIT_SUCCESS;
+}
+
+void runParser2(const std::string& query) {
+    CypherParser parser;
+    parser.allowNotImplemented(true);
+
+    try {
+        auto t0 = Clock::now();
+        parser.parse(query);
+        auto t1 = Clock::now();
+        fmt::print("Query parsed in {} us\n", duration<Microseconds>(t0, t1));
+    } catch (const ParserException& e) {
+        fmt::print("{}\n", e.what());
+    }
+}
--- samples/plan2/plan2.cpp
+++ samples/plan2/plan2.cpp
@@ -1,22 +1,36 @@
 #include <stdlib.h>
 
-#include "CypherParser.h"
+#include <iostream>
+
+#include "SystemManager.h"
 #include "Time.h"
+#include "PlanGraphDebug.h"
+#include "Graph.h"
+#include "SimpleGraph.h"
+#include "versioning/Transaction.h"
+#include "views/GraphView.h"
+#include "PlanGraphGenerator.h"
+#include "PlanGraph.h"
+#include "QueryAnalyzer.h"
+#include "QueryParser.h"
+#include "ASTContext.h"
+#include "QueryCommand.h"
 #include "ParserException.h"
+#include "AnalyzeException.h"
 #include "FileReader.h"
 
 using namespace db;
 
-void runParser2(const std::string& query);
+void runPlan2(std::string_view query);
 
 int main(int argc, char** argv) {
     std::string queryStr;
 
-    if (argc > 1 && strlen(argv[1]) > 0) {
+    if (argc == 2) {
         queryStr = argv[1];
 
     } else {
-        fs::Path path(SAMPLE_DIR "/parser-queries.txt");
+        fs::Path path(SAMPLE_DIR "/query.txt");
         fmt::print("Reading query from file: {}\n", path.get());
         fs::File file = fs::File::open(path).value();
         fs::FileReader reader;
@@ -29,28 +43,49 @@ int main(int argc, char** argv) {
         queryStr = it.get<char>(file.getInfo()._size);
     }
 
-    runParser2(queryStr);
+    runPlan2(queryStr);
 
     return EXIT_SUCCESS;
 }
 
-void runParser2(const std::string& query) {
-    CypherParser parser;
-    parser.allowNotImplemented(false);
+void runPlan2(std::string_view query) {
+    SystemManager sysMan;
+    Graph* graph = sysMan.createGraph("simpledb");
+    SimpleGraph::createSimpleGraph(graph);
 
+    const Transaction transaction = graph->openTransaction();
+    const GraphView view = transaction.viewGraph();
+
+    auto callback = [](const Block& block) {};
+
+    ASTContext ctxt;
+    QueryParser parser(&ctxt);
+
+    QueryCommand* queryCmd {nullptr};
     try {
         auto t0 = Clock::now();
-        parser.parse(query);
-        auto t1 = Clock::now();
-        fmt::print("Query parsed in {} us\n", duration<Microseconds>(t0, t1));
+        queryCmd = parser.parse(query);
+        if (!queryCmd) {
+            std::cerr << "Failed to parse query" << std::endl;
+            return;
+        }
+        fmt::print("Query parsed in {} us\n", duration<Microseconds>(t0, Clock::now()));
     } catch (const ParserException& e) {
-        fmt::print("{}\n", e.what());
+        std::cerr << "Syntax error: " << e.what() << std::endl;
+        return;
     }
 
-    // const auto& queries = ast.queries();
-    // for (const auto& query : queries) {
-    //     // TODO Analyze
-    //     // Plan
-    //     // Execute
-    // }
+    QueryAnalyzer analyzer(view, &ctxt);
+    try {
+        analyzer.analyze(queryCmd);
+    } catch (const AnalyzeException& e) {
+        std::cerr << "Analyze error: " << e.what() << std::endl;
+        return;
+    }
+
+    PlanGraphGenerator planGen(view, callback);
+    planGen.generate(queryCmd);
+    const PlanGraph& planGraph = planGen.getPlanGraph();
+
+    PlanGraphDebug::dumpMermaid(std::cout, view, planGraph);
 }
