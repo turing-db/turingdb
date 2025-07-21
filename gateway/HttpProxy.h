acfa1c3f0156f861bd0953c862c70474d9edb4e9
suhas <suhas.narreddy@turing.bio>
2025-07-21 09:12:39 +0000

    Add Formatting Changes (!90)

--- gateway/HttpProxy.h
+++ gateway/HttpProxy.h
@@ -10,44 +10,44 @@
 #include "AddressRouter.h"
 
 
-template<typename T>
+template <typename T>
 concept ServerType = std::same_as<T, httplib::Server> || std::same_as<T, httplib::SSLServer>;
 
 class HttpProxyBase {
 public:
     virtual ~HttpProxyBase() = default;
 
-    virtual void setBackendServers(const std::vector<std::pair<std::string, int>>& backendList)=0;
+    virtual void setBackendServers(const std::vector<std::pair<std::string, int>>& backendList) = 0;
 
-    virtual void setupRoutes()=0;
+    virtual void setupRoutes() = 0;
 
-    virtual void start(const std::string& host, int port, int numThreads)=0;
+    virtual void start(const std::string& host, int port, int numThreads) = 0;
 
-    virtual void stop()=0;
+    virtual void stop() = 0;
 
-    virtual LoadBalancer& getLoadBalancer()=0; 
-    virtual const LoadBalancer& getLoadBalancer() const=0; 
+    virtual LoadBalancer& getLoadBalancer() = 0;
+    virtual const LoadBalancer& getLoadBalancer() const = 0;
 
-    virtual TokenValidator& getTokenValidator()=0; 
-    virtual const TokenValidator& getTokenValidator() const=0; 
+    virtual TokenValidator& getTokenValidator() = 0;
+    virtual const TokenValidator& getTokenValidator() const = 0;
 
-    virtual AddressRouter& getAddressRouter()=0; 
-    virtual const AddressRouter& getAddressRouter() const=0; 
+    virtual AddressRouter& getAddressRouter() = 0;
+    virtual const AddressRouter& getAddressRouter() const = 0;
     virtual bool isValid() const = 0;
-
 };
 
-template<ServerType Server>
-class HttpProxy: public HttpProxyBase{
+template <ServerType Server>
+class HttpProxy : public HttpProxyBase {
 public:
-    HttpProxy() requires std::same_as<Server, httplib::Server>
+    HttpProxy()
+        requires std::same_as<Server, httplib::Server>
     {
         setupRoutes();
     }
 
-    HttpProxy(const std::string& cert, const std::string& key) 
-    requires std::same_as<Server, httplib::SSLServer>
-    :_server(cert.data(),key.data())
+    HttpProxy(const std::string& cert, const std::string& key)
+        requires std::same_as<Server, httplib::SSLServer>
+        :_server(cert.data(), key.data()) 
     {
         setupRoutes();
     }
