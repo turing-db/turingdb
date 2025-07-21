88be7e0064a3bc5caf45f222ad9096a2c766bcb8
luclabarriere <luc.labarriere@turing.bio>
2025-07-21 11:27:52 +0000

    Made Statement and SubStatement abstract classes (!81)

--- v2/parser/statements/Statement.h
+++ v2/parser/statements/Statement.h
@@ -5,12 +5,14 @@ namespace db {
 class Statement {
 public:
     Statement() = default;
-    virtual ~Statement() = default;
+    virtual ~Statement() = 0;
 
     Statement(const Statement&) = default;
     Statement& operator=(const Statement&) = default;
     Statement(Statement&&) = default;
     Statement& operator=(Statement&&) = default;
 };
+
+inline Statement::~Statement() = default;
 
 }
--- v2/parser/statements/SubStatement.h
+++ v2/parser/statements/SubStatement.h
@@ -5,12 +5,14 @@ namespace db {
 class SubStatement {
 public:
     SubStatement() = default;
-    virtual ~SubStatement() = default;
+    virtual ~SubStatement() = 0;
 
     SubStatement(const SubStatement&) = default;
     SubStatement& operator=(const SubStatement&) = default;
     SubStatement(SubStatement&&) = default;
     SubStatement& operator=(SubStatement&&) = default;
 };
+
+inline SubStatement::~SubStatement() = default;
 
 }
