8209316d5765d9cd4d551c20ef7e8cf890ebdfde
cyrusknopf <cyrus.knopf@turing.bio>
2025-08-07 19:22:20 +0100

    Add space checking, works for simple graph (!100)

--- dump/StringIndexerDumpConstants.h
+++ dump/StringIndexerDumpConstants.h
@@ -4,15 +4,12 @@ namespace db {
 
 class StringIndexDumpConstants {
 public:
-    static constexpr size_t NODESIZE =
-        sizeof(char)      // _val
-        + sizeof(uint8_t) // _isComplete
-        + sizeof(size_t)  // _owners.size()
-        + ((StringIndex::PrefixTreeNode::ALPHABET_SIZE + 7) / 8)
-              * sizeof(uint8_t); // Child bitmap
-
-    // One bit for each child
-    static constexpr size_t CHILDMASKSIZE =
-        (StringIndex::PrefixTreeNode::ALPHABET_SIZE + 7) / 8;
+    static constexpr size_t MAXNODESIZE =
+        sizeof(size_t) + // NodeID
+        sizeof(size_t) + // Number of non-null children
+        // Worse case: all children, for each write index in child array and ID in
+        // @ref _nodeManager
+        (StringIndex::PrefixTreeNode::ALPHABET_SIZE) * (sizeof(size_t) + sizeof(size_t))
+        + sizeof(size_t); // Size of owners array
 };
 }
