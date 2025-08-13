59c28fe4d571b95ef10f009148e782ad5ae5bbf8
remy <remy.boutonnet@turing.bio>
2025-08-13 16:56:45 +0000

    Add test for creating 10 changes sequentially

---
+++ regress/change_10/main.py
@@ -0,0 +1,13 @@
+from turingdb import TuringDB
+
+client = TuringDB(host="http://localhost:6666")
+graph = "test_graph4"
+client.query(f"CREATE GRAPH {graph}")
+client.set_graph(graph)
+
+for i in range(11):
+    change = client.query("CHANGE NEW")["Change ID"][0]
+    client.checkout(change=str(change))
+    print(f"Submitting change {change}")
+    client.query("CHANGE SUBMIT")
+    client.checkout()
