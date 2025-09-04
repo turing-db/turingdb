e688e932645f787a262ef57fca1ca6fb5c11813c
cyrusknopf <cyrus.knopf@turing.bio>
2025-09-04 11:17:26 +0100

    Move to non-static version (!delete)

--- storage/versioning/DataPartModifier.cpp
+++ storage/versioning/DataPartModifier.cpp
@@ -6,25 +6,32 @@
 
 using namespace db;
 
-void DataPartModifier::deleteNodes(const WeakArc<DataPart> oldDP, WeakArc<DataPart> newDP,
-                                  const std::set<NodeID> toDelete) {
-    // TODO: Work out NodeID mapping function
+void DataPartModifier::prepare() {
+    // 1. Work out which edges need to be deleted
 
-    // The only nodes whose ID changes are those which have an ID greater than a deleted
-    // node, as whenever a node is deleted, all subsequent nodes need to have their ID
-    // reduced by 1 to fill the gap left by the deleted node. Thus, the new ID of a node,
-    // x, is equal to the number of deleted nodes which have an ID smaller than x.
-    [[maybe_unused]] auto nodeIDMapping = [&toDelete](NodeID x) {
-        auto smallerDeletedNodes = std::ranges::lower_bound(toDelete, x);
-        size_t numSmallerDeletedNodes = std::distance(smallerDeletedNodes, toDelete.cbegin());
+    // 2. Calculate NodeID mapping function
+
+    // The only nodes whose ID changes are those which have an ID greater than a
+    // deleted node, as whenever a node is deleted, all subsequent nodes need to have
+    // their ID reduced by 1 to fill the gap left by the deleted node. Thus, the new
+    // ID of a node, x, is equal to the number of deleted nodes which have an ID
+    // smaller than x.
+    _nodeIDMapping = [this](NodeID x) {
+        auto smallerDeletedNodesIt = std::ranges::lower_bound(_nodesToDelete, x);
+        size_t numSmallerDeletedNodes =
+            std::distance(smallerDeletedNodesIt, _nodesToDelete.cbegin());
         return x - numSmallerDeletedNodes;
     };
 
-    newDP->_nodes = NodeContainerModifier::deleteNodes(oldDP->nodes(), toDelete);
+    // 3. Calculate EdgeID mapping function
+}
+
+void DataPartModifier::deleteNodes() {
+    // TODO: Work out NodeID mapping function
+
+    _newDP->_nodes = NodeContainerModifier::deleteNodes(_oldDP->nodes(), _nodesToDelete);
 
     // TODO: Update EdgeContainer (delete edges where an incident node has been deleted)
     // TODO: Update PropertyManager
     // TODO: Update StringIndex
-
-    return;
 }
--- storage/versioning/DataPartModifier.h
+++ storage/versioning/DataPartModifier.h
@@ -4,18 +4,57 @@
 
 #include "ArcManager.h"
 #include "DataPart.h"
+#include "ID.h"
+#include "TuringException.h"
 
 namespace db {
 
 class DataPartModifier {
 public:
+    DataPartModifier(const DataPartModifier&) = delete;
+    DataPartModifier& operator=(const DataPartModifier&) = delete;
+    DataPartModifier(DataPartModifier&&) = delete;
+    DataPartModifier& operator=(DataPartModifier&&) = delete;
+
+    ~DataPartModifier() = default;
+
+    void applyDeletions();
+
     /**
      * @brief Modifies @ref newDP in place to remove all nodes with NodeIDs specified in
      * @ref toDelete
      * @detail TODO: Also updates other datastructures in DataParts to handle the updated
      * IDs of the nodes as a result of reassigning NodeIDs after deletion
      */
-    static void deleteNodes(const WeakArc<DataPart> oldDP, WeakArc<DataPart> newDP,
-                     const std::set<NodeID> toDelete);
+    void deleteNodes();
+
+private:
+    const WeakArc<DataPart> _oldDP {}; // nullptr
+    WeakArc<DataPart> _newDP {};       // nullptr
+
+    std::set<NodeID> _nodesToDelete;
+    std::set<EdgeID> _edgesToDelete;
+
+    std::function<NodeID(NodeID)> _nodeIDMapping {[](NodeID x) -> NodeID {
+        throw TuringException("Attempted to use uninitialised _nodeIDMapping");
+    }};
+
+    std::function<EdgeID(EdgeID)> _edgeIDMapping {[](EdgeID x) -> EdgeID {
+        throw TuringException("Attempted to use uninitialised _edgeIDMapping");
+    }};
+
+    void prepare();
+
+    // Copy constructor WeakArc to the old datapart so the old referee still holds a
+    // reference, but take ownership of WeakArc of the new datapart
+    DataPartModifier(const WeakArc<DataPart> oldDP, WeakArc<DataPart> newDP,
+                     std::set<NodeID> nodesToDelete, std::set<EdgeID> edgesToDelete)
+        : _oldDP(oldDP),
+          _newDP(std::move(newDP)),
+          _nodesToDelete(nodesToDelete),
+          _edgesToDelete(edgesToDelete)
+      {
+
+      }
 };
 }
