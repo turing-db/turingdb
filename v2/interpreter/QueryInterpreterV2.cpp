fae196bad21ca851116491a2286ab2bc65615383
remy <remy.boutonnet@turing.bio>
2025-09-05 14:19:06 +0000

    Integrate v2 components

--- v2/interpreter/QueryInterpreterV2.cpp
+++ v2/interpreter/QueryInterpreterV2.cpp
@@ -3,6 +3,14 @@
 #include "InterpreterContext.h"
 #include "SystemManager.h"
 #include "JobSystem.h"
+#include "versioning/Transaction.h"
+#include "CypherParser.h"
+#include "CypherAST.h"
+#include "CypherAnalyzer.h"
+#include "PlanGraphGenerator.h"
+#include "ParserException.h"
+#include "AnalyzeException.h"
+#include "PlannerException.h"
 
 #include "Profiler.h"
 
@@ -23,5 +31,68 @@ QueryStatus QueryInterpreterV2::execute(const InterpreterContext& ctxt,
                                         std::string_view graphName) {
     Profile profile {"QueryInterpreterV2::execute"};
 
-    return QueryStatus(QueryStatus::Status::OK);
+    const auto start = Clock::now();
+
+    auto txRes = _sysMan->openTransaction(graphName, ctxt.getCommitHash(), ctxt.getChangeID());
+    if (!txRes) {
+        switch (txRes.error().getType()) {
+            case ChangeErrorType::GRAPH_NOT_FOUND:
+                return QueryStatus(QueryStatus::Status::GRAPH_NOT_FOUND);
+            case ChangeErrorType::CHANGE_NOT_FOUND:
+                return QueryStatus(QueryStatus::Status::CHANGE_NOT_FOUND);
+            default:
+                return QueryStatus(QueryStatus::Status::COMMIT_NOT_FOUND);
+        }
+    }
+
+    auto view = txRes->viewGraph();
+
+    // Parsing query
+    CypherAST ast(query);
+    CypherParser parser(ast);
+    try {
+        parser.parse(query);
+    } catch (const ParserException& e) {
+        return QueryStatus(QueryStatus::Status::PARSE_ERROR, e.what());
+    } catch (const std::exception& e) {
+        return QueryStatus(QueryStatus::Status::PARSE_ERROR,
+                           std::string("Unexpected exception: ") + e.what());
+    } catch (...) {
+        return QueryStatus(QueryStatus::Status::PARSE_ERROR,
+                           "Unknown exception occurred");
+    }
+
+    // Analyze query
+    CypherAnalyzer analyzer(ast, view);
+    try {
+        analyzer.analyze();
+    } catch (const AnalyzeException& e) {
+        return QueryStatus(QueryStatus::Status::ANALYZE_ERROR, e.what());
+    } catch (const std::exception& e) {
+        return QueryStatus(QueryStatus::Status::ANALYZE_ERROR,
+                           std::string("Unexpected exception: ") + e.what());
+    } catch (...) {
+        return QueryStatus(QueryStatus::Status::ANALYZE_ERROR,
+                           "Unknown exception occurred");
+    }
+
+    // Generate plan graph
+    PlanGraphGenerator planGen(view, ctxt.getQueryCallback());
+    try {
+        
+    } catch (const PlannerException& e) {
+        return QueryStatus(QueryStatus::Status::PLAN_ERROR, e.what());
+    } catch (const std::exception& e) {
+        return QueryStatus(QueryStatus::Status::PLAN_ERROR,
+                           std::string("Unexpected exception: ") + e.what());
+    } catch (...) {
+        return QueryStatus(QueryStatus::Status::PLAN_ERROR,
+                           "Unknown exception occurred");
+    }
+
+    const auto end = Clock::now();
+
+    auto res = QueryStatus(QueryStatus::Status::OK);
+    res.setTotalTime(end - start);
+    return res;
 }
