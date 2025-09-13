23a1d37e2195fb3dc56764e9dc901fe89e61897c
remy <remy.boutonnet@turing.bio>
2025-09-13 12:44:59 +0000

    Add v2 binary for full flow

---
+++ samples/v2/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <memory>
+
+#include "CypherASTDumper.h"
+#include "CypherAnalyzer.h"
+#include "CypherParser.h"
+#include "Graph.h"
+#include "TuringTime.h"
+#include "ParserException.h"
+#include "FileReader.h"
+#include "SimpleGraph.h"
+#include "CypherAST.h"
+#include "AnalyzeException.h"
+#include "versioning/Transaction.h"
+
+using namespace db;
+using namespace db::v2;
+
+int main(int argc, char** argv) {
+    std::string queryStr;
+
+    std::unique_ptr<Graph> graph = Graph::create();
+    SimpleGraph::createSimpleGraph(graph.get());
+
+    const Transaction transaction = graph->openTransaction();
+    const GraphView view = transaction.viewGraph();
+
+    if (argc > 1 && strlen(argv[1]) > 0) {
+        queryStr = argv[1];
+
+    } else {
+        fs::Path path(SAMPLE_DIR "/queries.txt");
+        fmt::print("Reading query from file: {}\n", path.get());
+        fs::File file = fs::File::open(path).value();
+        fs::FileReader reader;
+
+        reader.setFile(&file);
+        reader.read();
+
+        auto it = reader.iterateBuffer();
+
+        queryStr = it.get<char>(file.getInfo()._size);
+    }
+
+    CypherAST ast(queryStr);
+    ast.setDebugLocations(true);
+
+    {
+        CypherParser parser(&ast);
+        parser.allowNotImplemented(false);
+
+        try {
+            auto t0 = Clock::now();
+            parser.parse(queryStr);
+            auto t1 = Clock::now();
+            fmt::print("Query parsed in {} us\n", duration<Microseconds>(t0, t1));
+        } catch (const ParserException& e) {
+            fmt::print("{}\n", e.what());
+            return 0;
+        }
+
+        CypherAnalyzer analyzer(&ast, view);
+
+        try {
+            auto t0 = Clock::now();
+            analyzer.analyze();
+            auto t1 = Clock::now();
+            fmt::print("Query analyzed in {} us\n", duration<Microseconds>(t0, t1));
+        } catch (const AnalyzeException& e) {
+            fmt::print("{}\n", e.what());
+            return EXIT_FAILURE;
+        }
+
+
+        CypherASTDumper dumper(analyzer.getAST());
+        dumper.dump(std::cout);
+    }
+
+    return EXIT_SUCCESS;
+}
