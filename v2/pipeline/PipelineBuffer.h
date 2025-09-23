5f44972f76722b17ace0f9f08c1110cec2fa113a
remy <remy.boutonnet@turing.bio>
2025-09-23 13:48:32 +0000

    Use Block inside PipelineBuffer

--- v2/pipeline/PipelineBuffer.h
+++ v2/pipeline/PipelineBuffer.h
@@ -1,8 +1,6 @@
 #pragma once
 
-namespace db {
-class Column;
-}
+#include "columns/Block.h"
 
 namespace db::v2 {
 
@@ -19,27 +17,21 @@ public:
     bool hasData() const { return _hasData; }
     bool canWriteData() const { return !_hasData; }
 
-    Column* getColumn() const { return _col; }
-    void setColumn(Column* column) { _col = column; }
+    Block& getBlock() { return _block; }
+    const Block& getBlock() const { return _block; }
 
     void setSource(Processor* source) { _source = source; }
     void setConsumer(Processor* consumer) { _consumer = consumer; }
 
-    Column* consume() {
-        _hasData = false;
-        return _col;
-    }
-
-    void writeData() {
-        _hasData = true;
-    }
+    void consume() { _hasData = false; }
+    void writeData() { _hasData = true; }
 
     static PipelineBuffer* create(PipelineV2* pipeline);
 
 private:
     Processor* _source {nullptr};
     Processor* _consumer {nullptr};
-    Column* _col {nullptr};
+    Block _block;
     bool _hasData {false};
 
     PipelineBuffer() = default;
