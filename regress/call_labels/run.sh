9f9b4e67055646e2db1d81a4dda6cb06a328cff2
remy <remy.boutonnet@turing.bio>
2025-10-02 12:18:07 +0000

    Add regress test for CALL labels()

---
+++ regress/call_labels/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+pkill turingdb
+turingdb -in-memory
+
+rm -f pyproject.toml
+uv init
+uv add turingdb
+
+uv run main.py
+testres=$?
+
+pkill turingdb
+
+exit $testres
