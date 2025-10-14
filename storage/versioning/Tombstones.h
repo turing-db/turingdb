bf9af2268b1eb32302a3feff4353eea8f545b456
cyrusknopf <cyrus.knopf@turing.bio>
2025-10-14 17:59:37 +0100

    Add Tombstones methods (!tombstones)

--- storage/versioning/Tombstones.h
+++ storage/versioning/Tombstones.h
@@ -8,6 +8,21 @@ class Tombstones {
 public:
     using NodeTombstones = TombstoneSet<NodeID>;
     using EdgeTombstones = TombstoneSet<EdgeID>;
+
+    Tombstones() = default;
+    ~Tombstones() = default;
+
+    // Keep copy constructors for creating tombstones for next commit
+    Tombstones(const Tombstones&) = default;
+    Tombstones& operator=(const Tombstones&) = default;
+
+    // Delete move constructors as previous commits need to keep their tombstones
+    Tombstones(Tombstones&&) = delete;
+    Tombstones& operator=(Tombstones&&) = delete;
+
+    bool contains(NodeID nodeID) { return _nodeTombstones.contains(nodeID); }
+    bool contains(EdgeID edgeID) { return _edgeTombstones.contains(edgeID); }
+
 private:
     NodeTombstones _nodeTombstones;
     EdgeTombstones _edgeTombstones;
