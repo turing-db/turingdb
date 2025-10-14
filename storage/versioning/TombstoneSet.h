a1695f6e44a9a6913cfff42f44c4685a9976ff65
cyrusknopf <cyrus.knopf@turing.bio>
2025-10-14 17:47:27 +0100

    Add methods to TombstoneSet (!tombstones)

--- storage/versioning/TombstoneSet.h
+++ storage/versioning/TombstoneSet.h
@@ -9,8 +9,21 @@ namespace db {
 template <TypedInternalID IDT>
 class TombstoneSet {
 public:
+    bool contains(IDT id);
+    auto insert(IDT id);
+
 private:
     std::unordered_set<IDT> _set;
 };
+
+template <TypedInternalID IDT>
+bool TombstoneSet<IDT>::contains(IDT id) {
+    return _set.contains(id);
+}
+
+template <TypedInternalID IDT>
+auto TombstoneSet<IDT>::insert(IDT id) {
+    return _set.insert(id);
+}
 
 }
