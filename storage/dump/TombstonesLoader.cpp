e1d905f4f10860478e012a141544fdc728ed5832
cyrusknopf <cyrus.knopf@turing.bio>
2025-10-23 17:32:55 +0100

    Add TombstonesLoader

---
+++ storage/dump/TombstonesLoader.cpp
@@ -0,0 +1,52 @@
+#include "TombstonesLoader.h"
+
+#include "LoadUtils.h"
+#include "GraphDumpHelper.h"
+#include "versioning/Tombstones.h"
+
+using namespace db;
+
+DumpResult<void> TombstonesLoader::load(Tombstones& tombstones) {
+    _reader.nextPage();
+
+    if (_reader.errorOccured()) {
+        return DumpError::result(DumpErrorType::COULD_NOT_READ_TOMBSTONES,
+                                 _reader.error().value());
+    }
+
+    // Start reading metadata page
+    auto it = _reader.begin();
+    LoadUtils::ensureIteratorReadPage(it);
+
+    if (auto res = GraphDumpHelper::checkFileHeader(it); !res) {
+        return res.get_unexpected();
+    }
+
+    { // Load node tombstones
+        LoadUtils::ensureLoadSpace(sizeof(size_t), _reader, it);
+        const size_t nodeCount = it.get<size_t>();
+
+        // Load into vector first
+        std::vector<NodeID> tempNodeVec(nodeCount);
+        if (auto res = LoadUtils::loadVector(tempNodeVec, nodeCount, _reader, it); !res) {
+            return res;
+        }
+        // Insert all elements of vector into the tombstones set in one go
+        tombstones.nodeTombstones().insert(tempNodeVec);
+    }
+    
+    { // Load edge tombstones
+        LoadUtils::ensureLoadSpace(sizeof(size_t), _reader, it);
+        const size_t edgeCount = it.get<size_t>();
+
+        // Load into vector first
+        std::vector<EdgeID> tempEdgeVec(edgeCount);
+        if (auto res = LoadUtils::loadVector(tempEdgeVec, edgeCount, _reader, it); !res) {
+            return res;
+        }
+        // Insert all elements of vector into the tombstones set in one go
+        tombstones.edgeTombstones().insert(tempEdgeVec);
+    }
+
+    return {};
+}
