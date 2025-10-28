cbcf98a580bd3cce61dd019728c7245e24f501fb
cyrusknopf <cyrus.knopf@turing.bio>
2025-10-28 14:17:37 +0000

    Documentation (!tombstones)

--- storage/versioning/TombstoneSet.h
+++ storage/versioning/TombstoneSet.h
@@ -7,6 +7,13 @@
 
 namespace db {
 
+/**
+ * @brief Wrapper for datastructure which is used to store nodes/edges which are
+ * implicitly deleted, and filter the output of ChunkWriters based on these deletions.
+ * @detail Currently implemented using an unordered set, as latency-critical operation is
+ * lookups when filtering the output of chunk writers. Set theoretic union is also
+ * required - when rebasing - however this is less frequent. 
+ */
 template <TypedInternalID IDT>
 class TombstoneSet {
 public:
@@ -41,11 +48,6 @@ template <TypedInternalID IDT>
 template <std::ranges::input_range Range>
     requires std::same_as<std::ranges::range_value_t<Range>, IDT>
 void TombstoneSet<IDT>::insert(Range& range) {
-    // Justification of using @ref insert here: union of two unordered_sets may be slower
-    // than sorted vector, but the lookup for u_set is O(1) whilst sorted vec is O(logn).
-    // Therefore, using u_set is better for reads, but worse for writes, which aligns
-    // better to our philosphy than using sorted vec. (Source:
-    // https://lemire.me/blog/2017/01/27/how-expensive-are-the-union-and-intersection-of-two-unordered_set-in-c/)
     _set.insert(std::ranges::begin(range), std::ranges::end(range));
 }
 
