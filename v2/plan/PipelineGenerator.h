dc2603af60e4fd5ff7b48a111fe73f5c18664207
remy <remy.boutonnet@turing.bio>
2025-10-30 18:45:39 +0000

    Fix PipelineGenerator for v2 sample

--- v2/plan/PipelineGenerator.h
+++ v2/plan/PipelineGenerator.h
@@ -15,11 +15,13 @@ class PlanGraphStream;
 class VarNode;
 class ScanNodesNode;
 class GetOutEdgesNode;
+class GetEdgesNode;
 class GetEdgeTargetNode;
 class MaterializeNode;
 class NodeFilterNode;
 class EdgeFilterNode;
 class ProduceResultsNode;
+class JoinNode;
 
 class PipelineGenerator {
 public:
@@ -48,11 +50,13 @@ private:
     void translateVarNode(VarNode* node, PlanGraphStream& stream);
     void translateScanNodesNode(ScanNodesNode* node, PlanGraphStream& stream);
     void translateGetOutEdgesNode(GetOutEdgesNode* node, PlanGraphStream& stream);
+    void translateGetEdgesNode(GetEdgesNode* node, PlanGraphStream& stream);
     void translateGetEdgeTargetNode(GetEdgeTargetNode* node, PlanGraphStream& stream);
     void translateMaterializeNode(MaterializeNode* node, PlanGraphStream& stream);
     void translateNodeFilterNode(NodeFilterNode* node, PlanGraphStream& stream);
     void translateEdgeFilterNode(EdgeFilterNode* node, PlanGraphStream& stream);
     void translateProduceResultsNode(ProduceResultsNode* node, PlanGraphStream& stream);
+    void translateJoinNode(JoinNode* node, PlanGraphStream& stream);
 };
 
 }
