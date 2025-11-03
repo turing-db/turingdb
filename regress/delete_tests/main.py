887b8010ec245ffb005737611adc31739f897900
cyrusknopf <cyrus.knopf@turing.bio>
2025-11-03 17:41:11 +0000

    Add rebase tombstones regress test (!ssi)

--- regress/delete_tests/main.py
+++ regress/delete_tests/main.py
@@ -1,5 +1,14 @@
+from turingdb import TuringDB
+import rebased_tombstones_test as rebase_test
+
 def main() -> None:
-  ...
+  client = TuringDB(host="http://localhost:6666")
+  print("Connected to DB")
+
+  print("Running Tombstone rebase tests")
+  rebase_test.run(client)
+  print("Passed Tombstone rebase tests")
+  
 
 if __name__ == "__main__":
   main()
