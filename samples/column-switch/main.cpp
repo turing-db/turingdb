d6fce085a7500f191e0d3dcece10820a2e820f49
cyrusknopf <cyrus.knopf@turing.bio>
2025-11-10 11:52:50 +0000

    Add a working column dispatcher for vectors (!dispatcher)

--- samples/column-switch/main.cpp
+++ samples/column-switch/main.cpp
@@ -1,16 +1,15 @@
 #include <cstdio>
 
 #include "columns/ColumnSwitch.h"
+#include "spdlog/spdlog.h"
 
 using namespace db;
-
-#define CASE_COMPONENT(col, Type)                      \
-    { auto* c = static_cast<Type*>(col); }
 
 int main() {
     puts("0");
 
     Column* col = new ColumnVector<NodeID>({0, 1, 2, 3});
 
-    COLUMN_SWITCH(col);
+    dispatchColumn(col,
+                   [](auto* c) { spdlog::info("Vector is empty ? {}", c->empty()); });
 }
