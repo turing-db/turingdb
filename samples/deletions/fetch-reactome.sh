97f42769e220d66a25d223ecc4405de80e2f66d3
cyrusknopf <cyrus.knopf@turing.bio>
2025-11-15 15:11:22 +0000

    Remove reactome script (!ssi)

--- samples/deletions/fetch-reactome.sh
+++
@@ -1,3 +0,0 @@
-# Helper for running tests on reactome locally (we don't have reactome on CI currently)
-mkdir -p .turing/graphs
-ln -s ~/.turing/graphs/reactome ~/turingdb/samples/deletions/.turing/data/reactome
