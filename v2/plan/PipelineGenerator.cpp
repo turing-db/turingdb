be626a493cdb51ad0345868d18f94887c89b01ce
cyrusknopf <cyrus.knopf@turing.bio>
2025-11-18 14:41:05 +0000

    Remove PlanGraphStream (!pl-generator-binary)

--- v2/plan/PipelineGenerator.cpp
+++ v2/plan/PipelineGenerator.cpp
@@ -7,7 +7,6 @@
 #include "PendingOutputView.h"
 #include "PipelineInterface.h"
 #include "PlanGraph.h"
-#include "PlanGraphStream.h"
 
 #include "Projection.h"
 #include "nodes/CartesianProductNode.h"
@@ -35,12 +34,7 @@ using namespace db::v2;
 
 namespace {
 
-struct TranslateToken {
-    PlanGraphNode* node {nullptr};
-    PlanGraphStream stream;
-};
-
-using TranslateTokenStack = std::stack<TranslateToken>;
+using TranslateTokenStack = std::stack<PlanGraphNode*>;
 
 }
 
@@ -52,19 +46,19 @@ void PipelineGenerator::generate() {
     _graph->getRoots(rootNodes);
 
     for (const auto& node : rootNodes) {
-        nodeStack.emplace(node, PlanGraphStream());
+        nodeStack.push(node);
     }
 
     // Translate nodes in a DFS manner
     while (!nodeStack.empty()) {
-        auto [node, stream] = nodeStack.top();
+        PlanGraphNode* node = nodeStack.top();
         nodeStack.pop();
 
-        translateNode(node, stream);
+        translateNode(node);
 
         for (PlanGraphNode* nextNode : node->outputs()) {
             if (!nextNode->isBinary()) {
-                nodeStack.emplace(nextNode, stream);
+                nodeStack.push(nextNode);
                 continue;
             }
 
@@ -77,7 +71,7 @@ void PipelineGenerator::generate() {
 
             const bool visited = _binaryVisitedMap.contains(nextNode);
             if (visited) {
-                nodeStack.emplace(nextNode, stream);
+                nodeStack.push(nextNode);
                 continue;
             }
 
@@ -93,50 +87,51 @@ void PipelineGenerator::generate() {
     }
 }
 
-void PipelineGenerator::translateNode(PlanGraphNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateNode(PlanGraphNode* node) {
     switch (node->getOpcode()) {
         case PlanGraphOpcode::VAR:
-            translateVarNode(static_cast<VarNode*>(node), stream);
+            translateVarNode(static_cast<VarNode*>(node));
         break;
 
         case PlanGraphOpcode::SCAN_NODES:
-            translateScanNodesNode(static_cast<ScanNodesNode*>(node), stream);
+            translateScanNodesNode(static_cast<ScanNodesNode*>(node));
         break;
 
         case PlanGraphOpcode::GET_OUT_EDGES:
-            translateGetOutEdgesNode(static_cast<GetOutEdgesNode*>(node), stream);
+
+            translateGetOutEdgesNode(static_cast<GetOutEdgesNode*>(node));
         break;
 
         case PlanGraphOpcode::PRODUCE_RESULTS:
-            translateProduceResultsNode(static_cast<ProduceResultsNode*>(node), stream);
+            translateProduceResultsNode(static_cast<ProduceResultsNode*>(node));
         break;
 
         case PlanGraphOpcode::FILTER_NODE:
-            translateNodeFilterNode(static_cast<NodeFilterNode*>(node), stream);
+            translateNodeFilterNode(static_cast<NodeFilterNode*>(node));
         break;
 
         case PlanGraphOpcode::FILTER_EDGE:
-            translateEdgeFilterNode(static_cast<EdgeFilterNode*>(node), stream);
+            translateEdgeFilterNode(static_cast<EdgeFilterNode*>(node));
         break;
 
         case PlanGraphOpcode::SKIP:
-            translateSkipNode(static_cast<SkipNode*>(node), stream);
+            translateSkipNode(static_cast<SkipNode*>(node));
         break;
 
         case PlanGraphOpcode::LIMIT:
-            translateLimitNode(static_cast<LimitNode*>(node), stream);
+            translateLimitNode(static_cast<LimitNode*>(node));
         break;
 
         case PlanGraphOpcode::GET_EDGE_TARGET:
-            translateGetEdgeTargetNode(static_cast<GetEdgeTargetNode*>(node), stream);
+            translateGetEdgeTargetNode(static_cast<GetEdgeTargetNode*>(node));
         break;
 
         case PlanGraphOpcode::GET_IN_EDGES:
-            translateGetInEdgesNode(static_cast<GetInEdgesNode*>(node), stream);
+            translateGetInEdgesNode(static_cast<GetInEdgesNode*>(node));
         break;
 
         case PlanGraphOpcode::GET_EDGES:
-            translateGetEdgesNode(static_cast<GetEdgesNode*>(node), stream);
+            translateGetEdgesNode(static_cast<GetEdgesNode*>(node));
         break;
 
         case PlanGraphOpcode::GET_PROPERTY:
@@ -157,7 +152,7 @@ void PipelineGenerator::translateNode(PlanGraphNode* node, PlanGraphStream& stre
     }
 }
 
-void PipelineGenerator::translateVarNode(VarNode* node, PlanGraphStream&) {
+void PipelineGenerator::translateVarNode(VarNode* node) {
     const std::string_view varName = node->getVarDecl()->getName();
     if (varName.empty()) {
         throw PlannerException("VarNode with empty name");
@@ -182,30 +177,30 @@ void PipelineGenerator::translateVarNode(VarNode* node, PlanGraphStream&) {
     }
 }
 
-void PipelineGenerator::translateScanNodesNode(ScanNodesNode*, PlanGraphStream&) {
+void PipelineGenerator::translateScanNodesNode(ScanNodesNode* node) {
     _builder.addScanNodes();
 }
 
-void PipelineGenerator::translateGetOutEdgesNode(GetOutEdgesNode*, PlanGraphStream& ) {
+void PipelineGenerator::translateGetOutEdgesNode(GetOutEdgesNode* node) {
     _builder.addGetOutEdges();
 }
 
-void PipelineGenerator::translateGetInEdgesNode(GetInEdgesNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateGetInEdgesNode(GetInEdgesNode* node) {
     _builder.addGetInEdges();
 }
 
-void PipelineGenerator::translateGetEdgesNode(GetEdgesNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateGetEdgesNode(GetEdgesNode* node) {
     _builder.addGetEdges();
 }
 
-void PipelineGenerator::translateGetEdgeTargetNode(GetEdgeTargetNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateGetEdgeTargetNode(GetEdgeTargetNode* node) {
     _builder.projectEdgesOnOtherIDs();
 }
 
-void PipelineGenerator::translateNodeFilterNode(NodeFilterNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateNodeFilterNode(NodeFilterNode* node) {
 }
 
-void PipelineGenerator::translateEdgeFilterNode(EdgeFilterNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateEdgeFilterNode(EdgeFilterNode* node) {
 }
 
 void PipelineGenerator::translateProduceResultsNode(ProduceResultsNode* node, PlanGraphStream& stream) {
@@ -245,7 +240,7 @@ void PipelineGenerator::translateProduceResultsNode(ProduceResultsNode* node, Pl
     _builder.addLambda(lambdaCallback);
 }
 
-void PipelineGenerator::translateJoinNode(JoinNode* node, PlanGraphStream& stream) {
+void PipelineGenerator::translateJoinNode(JoinNode* node) {
 }
 
 void PipelineGenerator::translateSkipNode(SkipNode* node, PlanGraphStream& stream) {
@@ -298,8 +293,8 @@ void PipelineGenerator::translateLimitNode(LimitNode* node, PlanGraphStream& str
     _builder.addLimit(static_cast<size_t>(integerLiteral->getValue()));
 }
 
-void PipelineGenerator::translateCartesianProductNode(CartesianProductNode* node, PlanGraphStream&) {
-    if (_binaryVisitedMap.contains(node)) {
+void PipelineGenerator::translateCartesianProductNode(CartesianProductNode* node) {
+    if (!_binaryVisitedMap.contains(node)) {
         throw PipelineException("Attempted to translate CartesianProductNode which was "
                                 "not already encountered.");
     }
