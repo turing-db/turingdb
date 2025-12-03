cb5fd3647f0f74ca3eda309c7f952eb95e390459
luclabarriere <luc.labarriere@turing.bio>
2025-12-03 08:15:52 +0000

    Moved ProcedureBlueprintMap methods to .cpp file

---
+++ v2/pipeline/procedures/ProcedureBlueprint.cpp
@@ -0,0 +1,25 @@
+#include "ProcedureBlueprint.h"
+
+#include "PipelineException.h"
+#include "BioAssert.h"
+
+using namespace db::v2;
+
+size_t ProcedureBlueprint::getReturnValueIndex(std::string_view name) const {
+    for (size_t i = 0; i < _returnValues.size(); ++i) {
+        if (_returnValues[i]._name == name) {
+            return i;
+        }
+    }
+
+    throw PipelineException("Column is not returned by the procedure");
+}
+
+void ProcedureBlueprint::returnAll(std::vector<YieldItem>& yieldItems) const {
+    for (const auto& item : _returnValues) {
+        msgbioassert(item._type != ProcedureReturnType::INVALID,
+                     "Invalid procedure return type");
+
+        yieldItems.emplace_back(item._name);
+    }
+}
--- v2/pipeline/procedures/ProcedureBlueprint.h
+++ v2/pipeline/procedures/ProcedureBlueprint.h
@@ -6,9 +6,6 @@
 #include "ProcedureData.h"
 #include "procedures/ProcedureReturnValues.h"
 
-#include "PipelineException.h"
-#include "BioAssert.h"
-
 namespace db {
 class NamedColumn;
 }
@@ -28,34 +25,19 @@ public:
     using ExecuteCallback = void (*)(Procedure&);
     using AllocCallback = std::unique_ptr<ProcedureData> (*)();
 
-    size_t getReturnValueIndex(std::string_view name) const {
-        for (size_t i = 0; i < _returnValues.size(); ++i) {
-            if (_returnValues[i]._name == name) {
-                return i;
-            }
-        }
-
-        throw PipelineException("Column is not returned by the procedure");
-    }
+    size_t getReturnValueIndex(std::string_view name) const;
 
     ProcedureReturnType getReturnValueType(size_t index) const {
         return _returnValues[index]._type;
     }
+
+    void returnAll(std::vector<YieldItem>& yieldItems) const;
 
     std::string_view _name;
     ExecuteCallback _execCallback = nullptr;
     AllocCallback _allocCallback = nullptr;
     ProcedureReturnValues _returnValues;
     bool _valid = false;
-
-    void returnAll(std::vector<YieldItem>& yieldItems) const {
-        for (const auto& item : _returnValues) {
-            msgbioassert(item._type != ProcedureReturnType::INVALID,
-                         "Invalid procedure return type");
-
-            yieldItems.emplace_back(item._name);
-        }
-    }
 };
 
 }
