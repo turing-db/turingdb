0a5cfcb28fbdb076bfeca1ce27e95e0c86561d12
cyrusknopf <cyrus.knopf@turing.bio>
2025-12-11 18:44:16 +0000

    Unary operators, enum to string

--- v2/AST/expr/Operators.h
+++ v2/AST/expr/Operators.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include "EnumToString.h"
+
 namespace db::v2 {
 
 enum class BinaryOperator : uint8_t {
@@ -21,18 +23,55 @@ enum class BinaryOperator : uint8_t {
     Mod,
     Pow,
     In,
+
+    _SIZE
 };
+
+using BinaryOperatorDescription = EnumToString<BinaryOperator>::Create<
+    EnumStringPair<BinaryOperator::Or, "OR">,
+    EnumStringPair<BinaryOperator::Xor, "XOR">,
+    EnumStringPair<BinaryOperator::And, "AND">,
+    EnumStringPair<BinaryOperator::NotEqual, "NOTEQUAL">,
+    EnumStringPair<BinaryOperator::Equal, "EQUAL">,
+    EnumStringPair<BinaryOperator::LessThan, "LESSTHAN">,
+    EnumStringPair<BinaryOperator::GreaterThan, "GREATERTHAN">,
+    EnumStringPair<BinaryOperator::LessThanOrEqual, "LESSTHANOREQUAL">,
+    EnumStringPair<BinaryOperator::GreaterThanOrEqual, "GREATERTHANOREQUAL">,
+    EnumStringPair<BinaryOperator::Add, "ADD">,
+    EnumStringPair<BinaryOperator::Sub, "SUB">,
+    EnumStringPair<BinaryOperator::Mult, "MULT">,
+    EnumStringPair<BinaryOperator::Div, "DIV">,
+    EnumStringPair<BinaryOperator::Mod, "MOD">,
+    EnumStringPair<BinaryOperator::Pow, "POW">,
+    EnumStringPair<BinaryOperator::In, "IN">
+>;
 
 enum class UnaryOperator : uint8_t {
     Not,
     Minus,
     Plus,
+
+    _SIZE
 };
+
+using UnaryOperatorDescription = EnumToString<UnaryOperator>::Create<
+    EnumStringPair<UnaryOperator::Not, "NOT">,
+    EnumStringPair<UnaryOperator::Minus, "MINUS">,
+    EnumStringPair<UnaryOperator::Plus, "PLUS">
+>;
 
 enum class StringOperator : uint8_t {
     StartsWith,
     EndsWith,
     Contains,
+
+    _SIZE
 };
+
+using StringOperatorDescription = EnumToString<StringOperator>::Create<
+    EnumStringPair<StringOperator::StartsWith, "STARTSWITH">,
+    EnumStringPair<StringOperator::EndsWith, "ENDSWITH">,
+    EnumStringPair<StringOperator::Contains, "CONTAINS">
+>;
 
 }
