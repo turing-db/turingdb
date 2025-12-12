9d36e2992df33334361c3ff0f89d54c5d276e11b
remy <remy.boutonnet@turing.bio>
2025-12-12 22:12:10 +0000

    Fix v2 sample

--- samples/v2/main.cpp
+++ samples/v2/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char** argv) {
     std::string queryStr;
 
     TuringConfig config;
+    config.setSyncedOnDisk(false);
     SystemManager sysMan(&config);
     Graph* graph = sysMan.createGraph("simpledb");
     SimpleGraph::createSimpleGraph(graph);
