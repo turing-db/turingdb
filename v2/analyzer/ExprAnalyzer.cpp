f363add9f40da6d9fa5f22d52f5dfd26bafbdd2a
cyrusknopf <cyrus.knopf@turing.bio>
2025-12-16 15:39:43 +0000

    Fix float->double issues

--- v2/analyzer/ExprAnalyzer.cpp
+++ v2/analyzer/ExprAnalyzer.cpp
@@ -103,6 +103,7 @@ void ExprAnalyzer::analyzeBinaryExpr(BinaryExpr* expr) {
             type = EvaluatedType::Bool;
 
             if (pair == TypePairBitset(EvaluatedType::Integer, EvaluatedType::Integer)
+                || pair == TypePairBitset(EvaluatedType::Double, EvaluatedType::Double)
                 || pair == TypePairBitset(EvaluatedType::String, EvaluatedType::String)
                 || pair == TypePairBitset(EvaluatedType::String, EvaluatedType::Char)
                 || pair == TypePairBitset(EvaluatedType::Char, EvaluatedType::Char)
