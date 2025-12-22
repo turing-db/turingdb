5fe5e2faa23cfe71a492b9d8ed5ee1ce966d57fc
cyrusknopf <cyrus.knopf@turing.bio>
2025-12-22 11:16:45 +0000

    Add commit then rebase test (!fix-rebase-bug)

--- test/v2/queries/ChangeQueriesTest.cpp
+++ test/v2/queries/ChangeQueriesTest.cpp
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <gtest/gtest.h>
+#include <initializer_list>
 #include <string_view>
 
 #include "TuringDB.h"
@@ -11,6 +13,7 @@
 #include "columns/ColumnOptVector.h"
 #include "metadata/PropertyType.h"
 #include "ID.h"
+#include "versioning/ChangeID.h"
 #include "versioning/Transaction.h"
 #include "reader/GraphReader.h"
 #include "dataframe/Dataframe.h"
@@ -37,6 +40,8 @@ protected:
     Graph* _graph {nullptr};
     ChangeID _currentChange {ChangeID::head()};
 
+    static constexpr std::string_view GET_PROPERTIES_QUERY =
+        "CALL db.propertyTypes() YIELD propertyType as property";
 
     GraphReader read() { return _graph->openTransaction().readGraph(); }
 
@@ -49,16 +54,20 @@ protected:
     }
 
     void submitCurrentChange() {
-        auto res = _db->query("change submit", _graphName, &_env->getMem(), CommitHash::head(), _currentChange);
+        auto res = _db->query("CHANGE SUBMIT", _graphName, &_env->getMem(), CommitHash::head(), _currentChange);
         ASSERT_TRUE(res);
         _currentChange = ChangeID::head();
     }
 
     void submitChange(ChangeID chid) {
-        auto res = _db->query("change submit", _graphName, &_env->getMem(), CommitHash::head(), chid);
+        auto res = _db->query("CHANGE SUBMIT", _graphName, &_env->getMem(), CommitHash::head(), chid);
 
         ASSERT_TRUE(res);
         _currentChange = ChangeID::head();
+    }
+
+    void setChange(ChangeID chid) {
+        _currentChange = chid;
     }
 
     auto queryV2(std::string_view query, auto callback) {
@@ -80,6 +89,38 @@ protected:
             }
         }
         return nullptr;
+    }
+
+    bool ensureProperties(std::initializer_list<std::string_view> props) {
+        bool allPropsFound {true};
+        
+        auto res = queryV2(GET_PROPERTIES_QUERY, [&](const Dataframe* df) {
+            ASSERT_TRUE(df);
+
+            NamedColumn* propCol = findColumn(df, "property");
+
+            ASSERT_TRUE(propCol);
+
+            auto* castedProps = propCol->as<ColumnVector<std::string_view>>();
+            ASSERT_TRUE(castedProps);
+
+            for (const auto& desired : props) {
+                bool found {false};
+                for (const auto& prop : *castedProps) {
+                    if (prop == desired) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    allPropsFound = false;
+                    return;
+                }
+            }
+        });
+
+        EXPECT_TRUE(res);
+        return allPropsFound;
     }
 };
 
@@ -278,5 +319,125 @@ TEST_F(ChangeQueriesTest, threeChangeRebase) {
 
         actual.print(std::cout);
         ASSERT_TRUE(expected.equals(actual));
+    }
+}
+
+TEST_F(ChangeQueriesTest, commitThenRebase) {
+    setWorkingGraph("default");
+
+    newChange();
+    ChangeID change1 = _currentChange;
+
+    { // Change 1 commits locally, does not submit
+        ASSERT_TRUE(queryV2(
+            R"(create (n:CHANGE1LABEL {id:1, changeid: "ONE", committed:true}))",
+            [](const Dataframe*) {}));
+
+        ASSERT_TRUE(queryV2("COMMIT", [](const Dataframe*) {}));
+
+        ASSERT_TRUE(queryV2(
+            R"(create (n:CHANGE1LABEL {cheeky:true, id:2, changeid: "ONE", committed:true}))",
+            [](const Dataframe*) {}));
+
+        ASSERT_TRUE(queryV2(
+            R"(create (n:CHANGE1LABEL {cheeky:true, id:3, changeid: "ONE", committed:true}))",
+            [](const Dataframe*) {}));
+
+        ASSERT_TRUE(queryV2("COMMIT", [](const Dataframe*) {}));
+        ASSERT_TRUE(ensureProperties({"id", "changeid", "committed"}));
+    }
+
+    newChange();
+    [[maybe_unused]] ChangeID change2 = _currentChange;
+
+    { // Change 2
+        ASSERT_TRUE(queryV2(
+            R"(create (n:CHANGE2LABEL {id:4, changeid: "TWO", committed:false}))",
+            [](const Dataframe*) {}));
+        ASSERT_TRUE(queryV2(
+            R"(create (n:CHANGE2LABEL {id:5, changeid: "TWO", committed:false}))",
+            [](const Dataframe*) {}));
+        submitCurrentChange();
+    }
+
+    { // Verify submission of Change 2
+        std::string_view matchQuery = "MATCH (n) RETURN n, n.id, n.changeid, n.committed";
+
+        using Rows = LineContainer<NodeID, int64_t, std::string_view, bool>;
+
+        Rows expected;
+        {
+            expected.add({0, 4, "TWO", false});
+            expected.add({1, 5, "TWO", false});
+        }
+
+        Rows actual;
+        {
+            auto res = queryV2(matchQuery, [&actual](const Dataframe* df) {
+                ASSERT_TRUE(df);
+                auto* ns = findColumn(df, "n")->as<ColumnNodeIDs>();
+                auto* ids = findColumn(df, "n.id")
+                                ->as<ColumnOptVector<types::Int64::Primitive>>();
+                auto* chids = findColumn(df, "n.changeid")
+                                  ->as<ColumnOptVector<types::String::Primitive>>();
+                auto* cmts = findColumn(df, "n.committed")
+                                 ->as<ColumnOptVector<types::Bool::Primitive>>();
+                ASSERT_TRUE(ns);
+                ASSERT_TRUE(ids);
+                ASSERT_TRUE(chids);
+                ASSERT_TRUE(cmts);
+
+                for (size_t row {0}; row < df->getRowCount(); row++) {
+                    actual.add(
+                        {ns->at(row), *ids->at(row), *chids->at(row), *cmts->at(row)});
+                }
+            });
+            ASSERT_TRUE(res);
+        }
+        EXPECT_TRUE(expected.equals(actual));
+
+        EXPECT_TRUE(ensureProperties({"id", "changeid", "committed"}));
+    }
+
+    { // Submit Change 1 and verify submission
+        submitChange(change1);
+
+        std::string_view matchQuery = "MATCH (n) RETURN n, n.id, n.changeid, n.committed";
+        using Rows = LineContainer<NodeID, int64_t, std::string_view, bool>;
+
+        Rows expected;
+        {
+            expected.add({0, 4, "TWO", false});
+            expected.add({1, 5, "TWO", false});
+            expected.add({2, 1, "ONE", true});
+            expected.add({3, 2, "ONE", true});
+            expected.add({4, 3, "ONE", true});
+        }
+
+        Rows actual;
+        {
+            auto res = queryV2(matchQuery, [&actual](const Dataframe* df) {
+                ASSERT_TRUE(df);
+                auto* ns = findColumn(df, "n")->as<ColumnNodeIDs>();
+                auto* ids = findColumn(df, "n.id")
+                                ->as<ColumnOptVector<types::Int64::Primitive>>();
+                auto* chids = findColumn(df, "n.changeid")
+                                  ->as<ColumnOptVector<types::String::Primitive>>();
+                auto* cmts = findColumn(df, "n.committed")
+                                 ->as<ColumnOptVector<types::Bool::Primitive>>();
+                ASSERT_TRUE(ns);
+                ASSERT_TRUE(ids);
+                ASSERT_TRUE(chids);
+                ASSERT_TRUE(cmts);
+
+                for (size_t row {0}; row < df->getRowCount(); row++) {
+                    actual.add(
+                        {ns->at(row), *ids->at(row), *chids->at(row), *cmts->at(row)});
+                }
+            });
+            ASSERT_TRUE(res);
+        }
+        EXPECT_TRUE(expected.equals(actual));
+        EXPECT_TRUE(ensureProperties({"id", "changeid", "committed", "cheeky"}));
     }
 }
