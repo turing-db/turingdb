d97c80d9ef27e4c4e5c8a77a911c4eb82c984cf1
remy <remy.boutonnet@turing.bio>
2026-01-05 12:27:59 +0000

    Remove v1

--- server/JsonEncoder.h
+++ server/JsonEncoder.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include "QueryCommand.h"
 #include "PayloadWriter.h"
-#include "ReturnField.h"
-#include "ReturnProjection.h"
 #include "metadata/PropertyType.h"
-#include "ChangeCommand.h"
 #include "ID.h"
 #include "columns/Block.h"
 #include "columns/Column.h"
@@ -42,127 +38,6 @@ namespace db {
 class JsonEncoder {
 public:
     JsonEncoder() = delete;
-
-    static void writeHeader(PayloadWriter& writer, const QueryCommand* cmd) {
-        writer.key("header");
-        writer.obj();
-        writer.key("column_names");
-        writer.arr();
-
-        switch (cmd->getKind()) {
-            case QueryCommand::Kind::MATCH_COMMAND: {
-                const auto& fields = static_cast<const MatchCommand*>(cmd)->getProjection()->returnFields();
-
-                for (const auto& field : fields) {
-                    if (field->getMemberType().isValid()) {
-                        writer.value(field->getName() + "." + field->getMemberName());
-                    } else {
-                        writer.value(field->getName() + field->getMemberName());
-                    }
-                }
-                writer.end();
-
-                writer.key("column_types");
-                writer.arr();
-
-                for (const auto& field : fields) {
-                    if (field->getMemberType().isValid()) {
-                        writer.value(ValueTypeName::value(field->getMemberType()._valueType));
-                    } else {
-                        writer.value(ValueTypeName::value(ValueType::UInt64));
-                    }
-                }
-            } break;
-
-            case QueryCommand::Kind::EXPLAIN_COMMAND: {
-                writer.value("Plan Explanation");
-                writer.end();
-
-                writer.key("column_types");
-                writer.arr();
-                writer.value(ValueTypeName::value(ValueType::String));
-            } break;
-
-            case QueryCommand::Kind::HISTORY_COMMAND: {
-                // JSON doesn't support the single escaped
-                // newline character
-                writer.end();
-
-                writer.key("column_types");
-                writer.arr();
-            } break;
-
-            case QueryCommand::Kind::CHANGE_COMMAND: {
-                const auto& changeCmd = static_cast<const ChangeCommand*>(cmd);
-                const auto changeOp = changeCmd->getChangeOpType();
-                if (changeOp == ChangeOpType::NEW) {
-                    writer.value("Change ID");
-                    writer.end();
-
-                    writer.key("column_types");
-                    writer.arr();
-                    writer.value(ValueTypeName::value(ValueType::UInt64));
-                } else {
-                    writer.end();
-                    writer.key("column_types");
-                    writer.arr();
-                }
-            } break;
-
-            case QueryCommand::Kind::CALL_COMMAND: {
-                const auto& callCmd = static_cast<const CallCommand*>(cmd);
-
-                for (const auto& name : callCmd->getColNames()) {
-                    writer.value(name);
-                }
-                writer.end();
-
-                writer.key("column_types");
-                writer.arr();
-
-                for (const auto& type : callCmd->getColTypes()) {
-                    writer.value(ValueTypeName::value(type));
-                }
-            } break;
-
-            case QueryCommand::Kind::LIST_GRAPH_COMMAND: {
-                writer.value("graph");
-                writer.end();
-
-                writer.key("column_types");
-                writer.arr();
-                writer.value(ValueTypeName::value(ValueType::String));
-            } break;
-
-            case QueryCommand::Kind::CREATE_COMMAND:
-            case QueryCommand::Kind::DELETE_NODES_COMMAND:
-            case QueryCommand::Kind::DELETE_EDGES_COMMAND:
-            case QueryCommand::Kind::COMMIT_COMMAND:
-            case QueryCommand::Kind::DATAPARTMERGE_COMMAND:
-            case QueryCommand::Kind::CREATE_GRAPH_COMMAND:
-            case QueryCommand::Kind::S3CONNECT_COMMAND:
-            case QueryCommand::Kind::S3TRANSFER_COMMAND:
-            case QueryCommand::Kind::IMPORT_GRAPH_COMMAND:
-            case QueryCommand::Kind::LOAD_GRAPH_COMMAND: {
-                writer.end();
-                writer.key("column_types");
-                writer.arr();
-            } break;
-        }
-        writer.end();
-        writer.end();
-
-        writer.key("data");
-        writer.arr();
-    }
-
-    static void writeBlock(PayloadWriter& writer, const Block& block) {
-        writer.arr();
-        for (const Column* col : block.columns()) {
-            writeColumn(writer, col);
-        }
-        writer.end();
-    }
 
     static void writeDataframeHeader(PayloadWriter& writer, const Dataframe& df) {
         writer.key("header");
