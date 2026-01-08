02594609334b71a6d52b9b2f45aafddba221e795
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-08 15:18:09 +0000

    Use tag for PropertyNull

--- storage/metadata/PropertyNull.h
+++ storage/metadata/PropertyNull.h
@@ -2,6 +2,11 @@
 
 #include <optional>
 
+/**
+ * @brief Default-initialisable wrapper for std::nullopt_t, for use in @ref
+ * ColumnConst<PropertyNull>, required for IS NULL or IS NOT NULL operations.
+ */
 struct PropertyNull {
-    static constexpr std::optional<int> _value {std::nullopt};
+    consteval PropertyNull() noexcept = default;
+    consteval operator std::nullopt_t() const noexcept { return std::nullopt; }
 };
