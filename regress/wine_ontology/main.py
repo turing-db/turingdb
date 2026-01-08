059789aa58e0bc0e81e12f065ad95b9c9c1459ae
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-08 16:55:59 +0000

    Add test for failing wine ontology (!regress-migration)

--- regress/wine_ontology/main.py
+++ regress/wine_ontology/main.py
@@ -13,5 +13,12 @@ change = t.query("CHANGE NEW")["changeID"][0]
 t.checkout(change=str(change))
 
 # Query
-with open('wine_ontology.cypher') as f: wine_create_query = f.read()
-res = t.query(wine_create_query)
+with open('wine_ontology.cypher') as f:
+  wine_create_query = f.read()
+
+try:
+  res = t.query(wine_create_query)
+except Exception as e:
+  assert str(e) == "Unexpected exception: Internal Error: "
+else:
+  assert False # If an exception wasn't raised: bug
