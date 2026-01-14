f0f499144c1598e04358e7f9eecb8602c1b07fac
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-14 11:15:24 +0000

    Add PredicateProgramGenerator (!pred-prog)

---
+++ query/plan/PredicateProgramGenerator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ExprProgramGenerator.h"
+
+namespace db {
+
+class PredicateProgramGenerator final : public ExprProgramGenerator {
+public:
+    PredicateProgramGenerator(PipelineGenerator* gen,
+                              ExprProgram* exprProg,
+                              const PendingOutputView& pendingOut)
+        : ExprProgramGenerator(gen, exprProg, pendingOut)
+    {
+    }
+    ~PredicateProgramGenerator() final = default;
+    
+    void generatePredicate(const Predicate* pred);
+    void addLabelConstraint(Column* lblsetCol, const LabelSet& lblConstraint);
+    void addEdgeTypeConstraint(Column* edgeTypeCol, const EdgeTypeID& typeConstr);
+};
+
+}
