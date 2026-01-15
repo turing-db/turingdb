41cea94f4c054665b42a843703fd8f020daa391c
remy <remy.boutonnet@turing.bio>
2026-01-15 19:39:59 +0000

    Move applyBufferedEdgeProperties() to cpp file
    Address PR review comment by moving the large function implementation
    from GMLGraphParser.h to a new GMLGraphSax.cpp file.

--- import/gml/GMLGraphParser.h
+++ import/gml/GMLGraphParser.h
@@ -207,43 +207,7 @@ public:
     bool onEdgeEnd() { return true; }
 
 private:
-    void applyBufferedEdgeProperties() {
-        for (const auto& prop : _bufferedEdgeProps) {
-            std::visit([this, &prop](auto&& val) {
-                using T = std::decay_t<decltype(val)>;
-                if constexpr (std::is_same_v<T, std::string>) {
-                    _currentPropName = prop.key;
-                    _currentPropName += " (String)";
-                    const auto propType = _metadata->getOrCreatePropertyType(
-                        _currentPropName, ValueType::String);
-                    _builder->addEdgeProperty<types::String>(
-                        *_currentEdge, propType._id, std::string(val));
-                } else if constexpr (std::is_same_v<T, uint64_t>) {
-                    _currentPropName = prop.key;
-                    _currentPropName += " (UInt64)";
-                    const auto propType = _metadata->getOrCreatePropertyType(
-                        _currentPropName, ValueType::UInt64);
-                    _builder->addEdgeProperty<types::UInt64>(
-                        *_currentEdge, propType._id, val);
-                } else if constexpr (std::is_same_v<T, int64_t>) {
-                    _currentPropName = prop.key;
-                    _currentPropName += " (Int64)";
-                    const auto propType = _metadata->getOrCreatePropertyType(
-                        _currentPropName, ValueType::Int64);
-                    _builder->addEdgeProperty<types::Int64>(
-                        *_currentEdge, propType._id, val);
-                } else if constexpr (std::is_same_v<T, double>) {
-                    _currentPropName = prop.key;
-                    _currentPropName += " (Double)";
-                    const auto propType = _metadata->getOrCreatePropertyType(
-                        _currentPropName, ValueType::Double);
-                    _builder->addEdgeProperty<types::Double>(
-                        *_currentEdge, propType._id, val);
-                }
-            }, prop.value);
-        }
-        _bufferedEdgeProps.clear();
-    }
+    void applyBufferedEdgeProperties();
 
     Graph* _graph {nullptr};
     std::unique_ptr<Change> _change;
