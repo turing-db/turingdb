6a0ebab5484e2747a6a2544380ff1d709089bbb0
luclabarriere <luc.labarriere@turing.bio>
2026-01-21 17:36:16 +0000

    Fixed json import bugs + added unit tests

--- vector/import/JsonImporter.cpp
+++ vector/import/JsonImporter.cpp
@@ -13,7 +13,14 @@ ImportResult<void> JsonImporter::import(BatchVectorCreate& batch,
     }
 
     JsonSaxParser parser(batch);
-    nlohmann::json::sax_parse(data, &parser, nlohmann::json::input_format_t::json, true, true);
+
+    try {
+        nlohmann::json::sax_parse(data, &parser, nlohmann::json::input_format_t::json, true, true);
+    } catch (const ImportException& e) {
+        return nonstd::make_unexpected(e.err());
+    } catch (const nlohmann::json::exception& e) {
+        return ImportError::result(ImportErrorCode::JSONParseError, e.what());
+    }
 
     return {};
 }
