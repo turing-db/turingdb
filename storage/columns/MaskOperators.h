0795a2d643029e343a84dde3d9a125f7957b320c
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-26 09:16:09 +0000

    Remove symlinks

--- storage/columns/MaskOperators.h
+++ storage/columns/MaskOperators.h
@@ -1,1 +1,141 @@
-/home/dev/turingdb/samples/colops/MaskOperators.h
+#pragma once
+
+#include <functional>
+#include <concepts>
+
+#include "columns/ColumnMask.h"
+#include "columns/ColumnVector.h"
+
+namespace db {
+
+
+template <typename F>
+concept UnaryMaskOp =
+    std::invocable<F, bool> && std::convertible_to<std::invoke_result_t<F, bool>, bool>;
+
+template <typename F>
+concept BinaryMaskOp = std::invocable<F, bool, bool>
+                    && std::convertible_to<std::invoke_result_t<F, bool, bool>, bool>;
+
+template <typename F>
+struct MaskOperator {
+    template <typename T, typename U>
+    inline decltype(auto) operator()(T&& a, U&& b)
+        requires BinaryMaskOp<F>
+    {
+        return F {}(std::forward<T>(a), std::forward<U>(b));
+    }
+
+    template <typename T>
+    inline decltype(auto) operator()(T&& a)
+        requires UnaryMaskOp<F>
+    {
+        return F {}(std::forward<T>(a));
+    }
+};
+
+using AND = MaskOperator<std::logical_and<>>;
+using OR  = MaskOperator<std::logical_or<>>;
+using NOT = MaskOperator<std::logical_not<>>;
+using Apply = MaskOperator<std::identity>;
+
+template <typename Op>
+struct MaskOpExecutor {
+    // Binary operator
+    static void apply(ColumnMask* res,
+                      const ColumnMask* lhs,
+                      const ColumnMask* rhs) {
+        bioassert(lhs->size() == rhs->size(), "Misshapen ColumnMasks.");
+        const size_t size = lhs->size();
+
+        res->resize(size);
+        auto& resd = res->getRaw();
+        const auto& lhsd = lhs->getRaw();
+        const auto& rhsd = rhs->getRaw();
+
+        auto op = Op {};
+        for (size_t i {0}; i < size; i++) {
+            resd[i] = op(lhsd[i], rhsd[i]);
+       }
+    }
+
+    // Unary operator
+    static void apply(ColumnMask* res, const ColumnMask* arg)
+    {
+        const size_t size = arg->size();
+
+        res->resize(size);
+        auto& resd = res->getRaw();
+        const auto& argd = arg->getRaw();
+
+        auto op = Op {};
+        for (size_t i {0}; i < size; i++) {
+            resd[i] = op(argd[i]);
+        }
+    }
+};
+
+struct MaskApplicator {
+    template <typename T>
+    static void apply(ColumnVector<T>* res,
+                      const ColumnVector<T>* src,
+                      const ColumnMask* mask) {
+       bioassert(src->size() == mask->size(), "Misshapen ColumnVector and ColumnMask.");
+       const size_t size = src->size();
+
+       res->clear();
+       res->reserve(size);
+
+       const auto& srcd = src->getRaw();
+       const auto& maskd = mask->getRaw();
+       for (size_t i = 0; i < size; i++) {
+           if (maskd[i]) {
+               res->push_back(srcd[i]);
+           }
+        }
+    }
+
+    template <typename T>
+    static void apply(ColumnVector<T>* res,
+                      const ColumnMask* mask,
+                      const ColumnVector<T>* src) {
+       bioassert(src->size() == mask->size(), "Misshapen ColumnMask and ColumnVector.");
+       const size_t size = src->size();
+
+       res->clear();
+       res->reserve(size);
+
+       const auto& srcd = src->getRaw();
+       const auto& maskd = mask->getRaw();
+       for (size_t i = 0; i < size; i++) {
+           if (maskd[i]) {
+               res->push_back(srcd[i]);
+           }
+        }
+    }
+};
+
+// Binary operation on masks
+template <typename Op>
+static inline void exec(ColumnMask* res, ColumnMask* lhs, ColumnMask* rhs) {
+    MaskOpExecutor<Op>::apply(res, lhs, rhs);
+}
+
+// Unary operation on masks
+template <typename Op>
+static inline void exec(ColumnMask* res, ColumnMask* arg) {
+    MaskOpExecutor<Op>::apply(res, arg);
+}
+
+// Applying masks to vectors
+template <typename Op, typename T>
+static inline void exec(ColumnVector<T>* res, ColumnVector<T>* src, ColumnMask* mask) {
+    MaskApplicator::apply(res, src, mask);
+}
+
+template <typename Op, typename T>
+static inline void exec(ColumnVector<T>* res, ColumnMask* mask, ColumnVector<T>* src) {
+    MaskApplicator::apply(res, src, mask);
+}
+
+}
