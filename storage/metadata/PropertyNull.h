278053de8330b3eb53e67c2ccce376a4656417a7
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-27 10:27:15 +0000

    PropertyNull compiles

--- storage/metadata/PropertyNull.h
+++ storage/metadata/PropertyNull.h
@@ -8,17 +8,37 @@ namespace db {
  * ColumnConst<PropertyNull>, required for IS NULL or IS NOT NULL operations.
  */
 struct PropertyNull {
-    consteval PropertyNull() noexcept = default;
-    consteval operator std::nullopt_t() const noexcept { return std::nullopt; }
+    constexpr PropertyNull() noexcept = default;
+    constexpr operator std::nullopt_t() const noexcept { return std::nullopt; }
 };
 
 template <typename T>
-constexpr inline bool operator==(const PropertyNull&, T&&) noexcept {
+constexpr inline bool operator==(const PropertyNull&, const std::optional<T>&) noexcept {
     return false;
 }
 
 template <typename T>
-constexpr inline bool operator==(T&&, const PropertyNull&) noexcept {
+constexpr inline bool operator==(const std::optional<T>&, const PropertyNull&) noexcept {
+    return false;
+}
+
+template <typename T>
+constexpr inline bool operator!=(const PropertyNull&, const std::optional<T>&) noexcept {
+    return true;
+}
+
+template <typename T>
+constexpr inline bool operator!=(const std::optional<T>&, const PropertyNull&) noexcept {
+    return true;
+}
+
+template <typename T>
+constexpr inline bool operator==(const PropertyNull&, const T&) noexcept {
+    return false;
+}
+
+template <typename T>
+constexpr inline bool operator==(const T&, const PropertyNull&) noexcept {
     return false;
 }
 
