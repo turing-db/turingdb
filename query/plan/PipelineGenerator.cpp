5e6b18b574e8a7a2e97980316d8602cb748b4cf6
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-30 17:12:45 +0000

    Begin start of expr program in return (!return-expr)

--- query/plan/PipelineGenerator.cpp
+++ query/plan/PipelineGenerator.cpp
@@ -685,6 +685,8 @@ PipelineOutputInterface* PipelineGenerator::translateProduceResultsNode(ProduceR
 
     const Projection* projNode = node->getProjection();
 
+    ExprProgram* exprProg = ExprProgram::create(_pipeline);
+    ExprProgramGenerator progGen(this, exprProg, _builder.getPendingOutput());
     // No projection can happen in the case of a Standalone call
     // in which case, we can simply output the whole dataframe
     if (projNode) {
@@ -760,6 +762,28 @@ PipelineOutputInterface* PipelineGenerator::translateProduceResultsNode(ProduceR
 
                 items.push_back({tag, *name});
             }
+
+            const Expr::Kind itemKind = item->getKind();
+            // FIXME: Other kinds need evaluation?
+            const bool needsEvaluation = itemKind == Expr::Kind::BINARY || itemKind == Expr::Kind::UNARY;
+            if (needsEvaluation) {
+                progGen.generateExpr(item);
+            }
+            // XXX: Need to insert a processor here
+
+            const auto findColIt = _declToColumn.find(decl);
+            if (findColIt == _declToColumn.end()) {
+                throw PlannerException(fmt::format("Unregistered variable {}.", decl->getName()));
+            }
+
+            const ColumnTag tag = findColIt->second;
+            const std::string_view name = item->getName();
+
+            const std::string_view repr = name.empty()
+                ? _sourceManager->getStringRepr((std::uintptr_t)item)
+                : name;
+
+            items.push_back({tag, repr});
         }
 
         _builder.addProjection(items);
