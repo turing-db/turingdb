b7633b95a3c43e00e00c45e3520795dc44bab2fd
cyrusknopf <cyrus.knopf@turing.bio>
2026-01-30 17:12:45 +0000

    Add expr program in PL Gen

--- query/plan/PipelineGenerator.cpp
+++ query/plan/PipelineGenerator.cpp
@@ -679,6 +679,8 @@ PipelineOutputInterface* PipelineGenerator::translateProduceResultsNode(ProduceR
 
     const Projection* projNode = node->getProjection();
 
+    ExprProgram* exprProg = ExprProgram::create(_pipeline);
+    ExprProgramGenerator progGen(this, exprProg, _builder.getPendingOutput());
     // No projection can happen in the case of a Standalone call
     // in which case, we can simply output the whole dataframe
     if (projNode) {
@@ -721,15 +723,28 @@ PipelineOutputInterface* PipelineGenerator::translateProduceResultsNode(ProduceR
                 const Expr* expr = *exprPtr;
                 const VarDecl* decl = expr->getExprVarDecl();
 
+                const Expr::Kind itemKind = item->getKind();
+                // FIXME: Other kinds need evaluation?
+                const bool needsEvaluation = itemKind == Expr::Kind::BINARY || itemKind == Expr::Kind::UNARY;
+                if (needsEvaluation) {
+                    progGen.generateExpr(item);
+                }
+
+
                 if (!decl) {
                     throw PlannerException(
                         "Projection item does not have "
                         "a variable declaration");
                 }
 
-                const ColumnTag tag = _declToColumn.at(decl);
-                const std::optional<std::string_view> name =
-                    projNode->getName(expr);
+                const auto findColIt = _declToColumn.find(decl);
+                if (findColIt == _declToColumn.end()) {
+                    throw PlannerException(fmt::format("Unregistered variable {}.", decl->getName()));
+                }
+                const ColumnTag tag = findColIt->second;
+
+                const std::optional<std::string_view> name = projNode->getName(item);
+
                 if (!name) {
                     continue;
                 }
@@ -1027,8 +1042,7 @@ PipelineOutputInterface* PipelineGenerator::translateProcedureEvalNode(Procedure
         inDf = prevOutput->getDataframe();
     }
 
-    size_t i = 0;
-    for (const auto* argExpr : *argExprs) {
+    for (size_t i {0}; const auto* argExpr : *argExprs) {
         const Column* col = nullptr;
 
         const VarDecl* argDecl = argExpr->getExprVarDecl();
