f080d73881f1100e797b62ca1b93541668a018f0
cyrusknopf <cyrus.knopf@turing.bio>
2026-02-02 11:14:02 +0000

    Add exception in PL gen (!return-expr)

--- query/analyzer/CypherAnalyzer.cpp
+++ query/analyzer/CypherAnalyzer.cpp
@@ -232,6 +232,20 @@ void CypherAnalyzer::analyze(const ReturnStmt* returnSt) {
 
         isAggregate |= item->isAggregate();
         hasGroupingKeys |= !item->isAggregate();
+
+        const bool isBinary = item->getKind() == Expr::Kind::BINARY;
+        if (isBinary) {
+            spdlog::info("Detect binary");
+            // throw AnalyzeException(
+                // "Binary expressions in RETURN clauses are not yet supported.");
+        }
+
+        const bool isUnary = item->getKind() == Expr::Kind::UNARY;
+        if (isUnary) {
+            spdlog::info("Detect unary");
+            // throw AnalyzeException(
+                // "Unary expressions in RETURN clauses are not yet supported.");
+        }
     }
 
     if (projection->isReturningAll()) {
