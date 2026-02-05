71d9633d3b35e2228ba0f614fccea5711e4e6d6c
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-02-05 15:57:51 +0000

    Fix ubuntu CI workflow file

--- build.sh
+++ build.sh
@@ -2,3 +2,11 @@
 
 source setup.sh
 uv build --wheel
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    uv add delocate
+    uvx --from delocate delocate-wheel -v -w wheel dist/turingdb-*.whl
+else
+    uv add auditwheel
+    uvx auditwheel repair dist/*.whl -w wheel
+fi
