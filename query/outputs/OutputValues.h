0238d88ed589b05be52febce2bd634a665f0d5e2
Luc Labarrière <luc.labarriere@turing.bio>
2026-02-16 09:50:35 +0100

    Moved the optional concepts/traits into a common/OptionalLike.h header

--- query/outputs/OutputValues.h
+++ query/outputs/OutputValues.h
@@ -1,70 +1,16 @@
 #pragma once
 
-#include <optional>
 #include <string_view>
 #include <type_traits>
 
 #include "ID.h"
+#include "OptionalLike.h"
 #include "metadata/PropertyType.h"
 #include "versioning/CommitHash.h"
 
 #include "Panic.h"
 
 namespace db {
-
-template <typename T>
-struct IsOptional : std::false_type {};
-
-template <typename T>
-struct IsOptional<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept Optional = IsOptional<T>::value;
-
-template <typename T>
-struct IsOptionalUnsignedInteger : std::false_type {};
-
-template <std::unsigned_integral T>
-struct IsOptionalUnsignedInteger<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept OptionalUnsignedInteger = IsOptionalUnsignedInteger<T>::value;
-
-template <typename T>
-struct IsOptionalSignedInteger : std::false_type {};
-
-template <std::signed_integral T>
-struct IsOptionalSignedInteger<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept OptionalSignedInteger = IsOptionalSignedInteger<T>::value;
-
-template <typename T>
-struct IsOptionalFloatingPoint : std::false_type {};
-
-template <std::floating_point T>
-struct IsOptionalFloatingPoint<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept OptionalFloatingPoint = IsOptionalFloatingPoint<T>::value;
-
-template <typename T>
-struct IsOptionalString : std::false_type {};
-
-template <std::convertible_to<std::string_view> T>
-struct IsOptionalString<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept OptionalString = IsOptionalString<T>::value;
-
-template <typename T>
-struct IsOptionalBool : std::false_type {};
-
-template <std::convertible_to<bool> T>
-struct IsOptionalBool<std::optional<T>> : std::true_type {};
-
-template <typename T>
-concept OptionalBool = IsOptionalBool<T>::value;
 
 template <typename T>
 struct IsHash : std::false_type {};
