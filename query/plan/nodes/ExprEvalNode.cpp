db08b4b1931afe3ca57de2a787038637d527b0be
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-02-17 13:25:40 +0100

    Add support for function expression function arguments

--- query/plan/nodes/ExprEvalNode.cpp
+++ query/plan/nodes/ExprEvalNode.cpp
@@ -1,5 +1,9 @@
 #include "ExprEvalNode.h"
 
+#include "expr/Expr.h"
+#include "Literal.h"
+#include "decl/EvaluatedType.h"
+
 using namespace db;
 
 ExprEvalNode::ExprEvalNode()
@@ -8,4 +12,25 @@ ExprEvalNode::ExprEvalNode()
 }
 
 ExprEvalNode::~ExprEvalNode() {
+}
+
+// Unary and Binary expressions need evaluating.
+// Literals that appear only on the RHS of a return, e.g. `RETURN 5`, need a
+// column allocated for them, otherwise they will not exist. @ref addExpr where
+// @ref item is a literal will allocate this column.
+// Wildcard (*) is of type LITERAL, but does not need evaluating.
+bool ExprEvalNode::needsEvaluation(const Expr* expr) {
+    const Expr::Kind exprKind = expr->getKind();
+
+    const bool operatorType =
+        exprKind == Expr::Kind::BINARY || exprKind == Expr::Kind::UNARY;
+
+    if (operatorType) {
+        return true;
+    }
+
+    const bool isLiteral = exprKind == Expr::Kind::LITERAL;
+    const bool isWildcard = expr->getType() == EvaluatedType::Wildcard;
+
+    return isLiteral && !isWildcard;
 }
