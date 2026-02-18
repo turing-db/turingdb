ebe4ab8baa5774cdf2f7046a3f3029697913dce3
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-02-18 15:26:00 +0100

    Add subsort first draft to OrderByProcessor

--- query/pipeline/processors/OrderByProcessor.cpp
+++ query/pipeline/processors/OrderByProcessor.cpp
@@ -5,12 +5,17 @@
 #include <numeric>
 
 #include <range/v3/action/sort.hpp>
+#include <range/v3/view/drop.hpp>
+#include <range/v3/view/subrange.hpp>
 #include <range/v3/view/transform.hpp>
 
 #include <spdlog/fmt/bundled/format.h>
 
 #include "PipelinePort.h"
 #include "columns/Column.h"
+#include "columns/ColumnOperatorDispatcher.h"
+
+#include "columns/AllowedKinds.h"
 
 #include "BioAssert.h"
 
@@ -21,18 +26,18 @@ namespace rv = rg::views;
 
 namespace {
 
-template <bool Ascending>
 struct OrderColumn {
     OrderByProcessor::Indices& _indices;
     OrderByProcessor::TieRanges& _ranges;
+    bool _ascending {true};
 
     template <typename T>
         requires std::totally_ordered<T>
-    void operator()(ColumnVector<T>& typed) {
-        std::vector<T>& data = typed.getRaw();
+    void operator()(const ColumnVector<T>* typed) {
+        const std::vector<T>& data = typed->getRaw();
 
         // Sort the indices by indexing the column
-        if constexpr (Ascending) {
+        if (_ascending) {
             rg::sort(_indices, [&](size_t i, size_t j) { return data[i] < data[j]; });
         } else {
             rg::sort(_indices, [&](size_t i, size_t j) { return data[i] > data[j]; });
@@ -42,25 +47,25 @@ struct OrderColumn {
         auto reordered =
             _indices | rv::transform([&](std::size_t i) -> auto& { return data[i]; });
 
-        OrderByProcessor::addTieRanges(_ranges, data);
+        OrderByProcessor::addTieRanges(_ranges, reordered);
     }
 };
 
-template <bool Ascending>
 struct OrderColumnSubrange {
     OrderByProcessor::Indices& _indices;
     size_t subrangeStart {0};
     size_t subrangeEnd {0};
+    bool _asc {true};
 
     template <typename T>
         requires std::totally_ordered<T>
-    void operator()(ColumnVector<T>& typed) {
-        std::vector<T>& data = typed.getRaw();
+    void operator()(const ColumnVector<T>* typed) {
+        const std::vector<T>& data = typed->getRaw();
 
         const auto beginIt = begin(_indices) + subrangeStart;
         const auto endIt = begin(_indices) + subrangeEnd;
 
-        if constexpr (Ascending) {
+        if (_asc) {
             std::sort(beginIt, endIt,
                       [&data](size_t i, size_t j) { return data[i] < data[j]; });
         } else {
@@ -76,8 +81,8 @@ struct NarrowTieRanges {
 
     template <typename T>
         requires std::totally_ordered<T>
-    void operator()(ColumnVector<T>& typed) {
-        std::vector<T>& data = typed->getRaw();
+    void operator()(const ColumnVector<T>* typed) {
+        const std::vector<T>& data = typed->getRaw();
 
         auto reordered =
             _indices | rv::transform([&](std::size_t i) -> auto& { return data[i]; });
@@ -90,8 +95,9 @@ struct NarrowTieRanges {
 
             const auto beginIt = begin(reordered) + start;
             const auto endIt = begin(reordered) + end;
+            const auto tiedRange = rg::subrange(beginIt, endIt);
 
-            OrderByProcessor::addTieRanges(temp, beginIt, endIt, start);
+            OrderByProcessor::addTieRanges(temp, tiedRange, start);
         }
 
         _ranges.swap(temp);
@@ -131,6 +137,7 @@ OrderByProcessor* OrderByProcessor::create(PipelineV2* pipeline,
         proc->_orderedKeys.reserve(keys.size());
         proc->_orderedKeys.assign(begin(keys), end(keys));
     }
+
     proc->postCreate(pipeline);
     return proc;
 }
@@ -170,15 +177,69 @@ void OrderByProcessor::subsort() {
     bioassert(sizeIt == end(_orderedKeys),
               "Attempted to sort non-equal length columns in ORDER BY");
 
-    Column* dominantCol = _orderedKeys.front()._col;
-    const size_t size = dominantCol->size();
+    { // Sort the entirety of the first column
+        const OrderByKey& dominantKey = _orderedKeys.front();
+        Column* dominantCol = dominantKey._col;
+        const bool asc = dominantKey._asc;
+        const size_t size = dominantCol->size();
 
-    _indices.resize(size);
-    std::ranges::iota(_indices, 0);
+        _indices.resize(size);
+        std::ranges::iota(_indices, 0);
+
+        OrderColumn sorter {
+            ._indices = _indices, ._ranges = _tieRanges, ._ascending = asc};
+
+        using Sort = ColumnSingleDispatcher<OrderedTypes::Allowed, OrderColumn,
+                                            OrderedTypes::Excluded>;
+
+        Sort::dispatch(dominantCol, sorter);
+    }
+
+    // If the ordering is completely determined by the first key (no tied-values), then
+    // nothing else to sort.
+    if (_tieRanges.empty()) {
+        // TODO: Project
+        return;
+    }
+
+    // Sort only the subspans of tied values (stored in @ref _tiedRanges) in the remaining
+    // columns
+
+
+    const auto& remainingKeys = _orderedKeys | rv::drop(1);
+
+    for (auto& [column, asc] : remainingKeys) {
+        // No ties: nothing left to sort
+        if (_tieRanges.empty()) {
+            break;
+        }
+
+        OrderColumnSubrange subrangeSorter {._indices = _indices, ._asc = asc};
+
+        for (const auto& [start, size] : _tieRanges) {
+            const size_t end = start + size;
+
+            subrangeSorter.subrangeStart = start;
+            subrangeSorter.subrangeEnd = end;
+
+            using SubrangeSort = ColumnSingleDispatcher<OrderedTypes::Allowed,
+                                                        OrderColumnSubrange,
+                                                        OrderedTypes::Excluded>;
+
+            SubrangeSort::dispatch(column, subrangeSorter);
+        }
+
+        NarrowTieRanges narrowTieRanges {._indices = _indices, ._ranges = _tieRanges};
+
+        using NarrowRanges = ColumnSingleDispatcher<OrderedTypes::Allowed,
+                                                    NarrowTieRanges,
+                                                    OrderedTypes::Excluded>;
+
+        NarrowRanges::dispatch(column, narrowTieRanges);
+    }
 }
 
-// TODO:
-// - Handle ColumnConst as order key
+// TODO: Handle ColumnConst as order key
 void OrderByProcessor::execute() {
     if (_state == State::SORT_INCOMING) {
         return;
