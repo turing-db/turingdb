402fe1621c94b269676da6b1348ae1167999802c
Luc Labarrière <luc.labarriere@turing.bio>
2026-02-26 12:29:01 +0100

    First implementation of the variable length paths in the planner

---
+++ query/plan/nodes/BFSExpandEdgesNode.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "PlanGraphNode.h"
+
+namespace db {
+
+class BFSExpandEdgesNode : public PlanGraphNode {
+public:
+    explicit BFSExpandEdgesNode(int64_t minHops, int64_t maxHops)
+        : PlanGraphNode(PlanGraphOpcode::BFS_EXPAND_EDGES),
+        _minHops(minHops),
+        _maxHops(maxHops)
+    {
+    }
+
+    int64_t getMinHops() const { return _minHops; }
+    int64_t getMaxHops() const { return _maxHops; }
+
+private:
+    int64_t _minHops {0};
+    int64_t _maxHops {0};
+};
+
+}
