6930bfdfec315b1feea4fe72885e2dbb73e0ef68
Luc Labarrière <luc.labarriere@turing.bio>
2026-03-04 13:07:56 +0100

    Now checking if port is closed instead of skipping the empty chunk condition. + fixed unit tests

--- query/pipeline/processors/LimitProcessor.cpp
+++ query/pipeline/processors/LimitProcessor.cpp
@@ -57,6 +57,11 @@ void LimitProcessor::execute() {
 
         // Write rows of inputBlock that are below the limit
         const size_t rowsToWrite = std::min(blockRowCount, remainingCapacity);
+        if (rowsToWrite == 0 && _input.getPort()->isClosed()) {
+            _reachedLimit = true;
+            return;
+        }
+
         outputDf->copyFromLine(inputDf, 0, rowsToWrite);
 
         if (blockRowCount >= remainingCapacity) {
