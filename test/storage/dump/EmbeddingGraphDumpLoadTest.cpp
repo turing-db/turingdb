2404a2e891d5c63274f4640d75d3c8858828d698
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-03-23 19:23:09 +0100

    Reduce gtest iterations for embedding tests

--- test/storage/dump/EmbeddingGraphDumpLoadTest.cpp
+++ test/storage/dump/EmbeddingGraphDumpLoadTest.cpp
@@ -197,5 +197,5 @@ TEST_F(EmbeddingGraphDumpLoadTest, MultiCommitEmbeddings) {
 }
 
 int main(int argc, char** argv) {
-    return turingTestMain(argc, argv, [] { testing::GTEST_FLAG(repeat) = 3; });
+    return turingTestMain(argc, argv, [] { testing::GTEST_FLAG(repeat) = 1; });
 }
