f7752d2903bb5243205e7f6b5011e043d1ddaab7
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-03-25 17:51:29 +0100

    Restore NameOf code to use other macros

--- common/NameOf.h
+++ common/NameOf.h
@@ -7,14 +7,16 @@ namespace db {
 template <typename T>
 struct NameOf {
     static std::string_view get() {
-#ifdef __clang__
+#if defined(__clang__)
         const std::string_view sv = __PRETTY_FUNCTION__;
         const size_t start = sv.find('[') + 5;
         const size_t end = sv.rfind(']');
-#elifdef __GNUC__
+
+#elif defined(__GNUC__)
         const std::string_view sv = __PRETTY_FUNCTION__;
         const size_t start = sv.find("T = ") + 4;
         const size_t end = sv.rfind(']');
+
 #endif
         return sv.substr(start, end - start);
     }
