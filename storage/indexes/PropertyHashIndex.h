791d20c57f9f230b25d5cbe8dd03a50c49e27f64
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-03-26 21:14:43 +0100

    Add query functions

--- storage/indexes/PropertyHashIndex.h
+++ storage/indexes/PropertyHashIndex.h
@@ -36,7 +36,7 @@ public:
 
     void init(GraphView view) final;
 
-    void query(const Column* query, Column* result) final;
+    void query(const Column* input, Column* result) final;
 
     size_t size() const final;
 
@@ -44,8 +44,10 @@ private:
     PropertyTypeID _propID;
     PropertyHashMap<PropertyPrimitive, IDContainer, MapType> _hashTable;
 
-    IDContainer _empty {};
     static constexpr bool isNode = std::is_same_v<I, NodeID>;
+
+    void query(const ColumnVector<PropertyPrimitive>* input, ColumnVector<I>* result);
+    void query(const ColumnConst<PropertyPrimitive>* input, ColumnVector<I>* result);
 };
 
 // Implementation of the underlying hashtable used for retrieval
@@ -58,6 +60,10 @@ public:
 
     auto begin() const { return _hashMap.begin(); }
     auto end() const { return _hashMap.end(); }
+
+    bool contains(const K& key) const { return _hashMap.contains(key); }
+
+    auto find(const K& key) const { return _hashMap.find(key); }
 
 private:
     HashMap _hashMap;
