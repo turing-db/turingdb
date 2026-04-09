fc1307876f9609fe6b8be5d0edfc1b88215a4541
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-04-09 12:55:52 +0200

    Add Null overload for add, find

--- storage/properties/PropertyContainer.h
+++ storage/properties/PropertyContainer.h
@@ -8,6 +8,7 @@
 
 #include "embedding/EmbeddingContainer.h"
 #include "StringContainer.h"
+#include "metadata/PropertyNull.h"
 #include "metadata/PropertyType.h"
 
 #include "ID.h"
@@ -69,6 +70,7 @@ protected:
     IDs _ids;
     bool _sorted {false};
     std::unordered_map<EntityID, size_t> _entityIndexMap;
+    static constexpr size_t NULL_INDEX = std::numeric_limits<size_t>::max();
 
 private:
     ValueType _valueType {ValueType::Invalid};
@@ -99,6 +101,10 @@ public:
         _sorted = false;
     }
 
+    void add(EntityID entityID, const PropertyNull&) {
+        _entityIndexMap[entityID] = NULL_INDEX;
+    }
+
     bool has(EntityID entityID) const override {
         auto it = find(entityID);
         return it != _values.end();
@@ -112,6 +118,10 @@ public:
         }
 
         const size_t offset = it->second;
+
+        if (offset == NULL_INDEX) {
+            return _values.end();
+        }
 
         return _values.begin() + offset;
     }
