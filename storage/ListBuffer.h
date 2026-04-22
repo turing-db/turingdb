bfdffa57b9be19a6b7cbd91184a3a3654479b511
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-04-22 16:06:47 +0200

    Document ListBuffer.h

--- storage/ListBuffer.h
+++ storage/ListBuffer.h
@@ -13,30 +13,52 @@
 
 namespace db {
 
+/**
+ * @brief Container to store heterogeneous lists. Guarantees elements that are
+ * inserted as a result of a single call to @ref insert are contiguous. References are
+ * permamently stable.
+ *
+ * @detail Stores list elements (in @ref _elements) alongside a non-owning view for each
+ * element (in @ref ListElementViewBuffer).
+ */
 template <size_t N = 4096>
 class ListBuffer {
 public:
+    /**
+     * @brief Given the provided @param elements, stores those elements in contiguous,
+     * stable memory, and returns a stable @ref ListView into each stored element.
+     */
     template <typename... Elements>
     ListView insert(const Elements&... elements);
 
 private:
-    ListByteBuffer<N> _buf;
+    /// Container of raw bytes for each element
+    ListByteBuffer<N> _elements;
+    /// Container of @ref ListElementView, for each element in @ref _elements
     ListElementViewBuffer<N> _views;
 };
 
 template <size_t N>
 template <typename... Elements>
 ListView ListBuffer<N>::insert(const Elements&... elements) {
-    constexpr size_t numBytes = (... + (ListByteBuffer<N>::tagSize() + sizeof(Elements)));
+    // For each element, calculate the bytes taken by the tag + the raw bytes of the type
+    constexpr size_t numBytes = ((ListByteBuffer<N>::tagSize() + sizeof(Elements)) + ...);
+    // Calculate how many elements we have to allocate the same number of views
     constexpr size_t numElements = sizeof...(elements);
 
-    _buf.reserveContiguous(numBytes);
+    // Ensure all the elements we are about to write are stored contigously
+    _elements.reserveContiguous(numBytes);
+    // Ensure all the views we are about to write are stored contigously
     _views.reserveContiguous(numElements);
 
+    // Before writing, calculate the address at which this list will start
     const ListElementView* listStart = _views.nextPtr();
 
-    (_views.write(_buf.write(TypeToListBufferTag<Elements>::Tag, elements)), ...);
+    // For each element: write its tag, followed by its value, and write the corresponding
+    // view for that element
+    (_views.write(_elements.write(TypeToListBufferTag<Elements>::Tag, elements)), ...);
 
+    // Return a span-like @ref ListView over the views for each element we just wrote
     return ListView {listStart, numElements};
 }
 
