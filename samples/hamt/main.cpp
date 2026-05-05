6f63297e6c891b2b78ff5c66dbcbbd90041b51a8
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-05-05 16:41:21 +0200

    Mutable insert and find working in trivial sample

--- samples/hamt/main.cpp
+++ samples/hamt/main.cpp
@@ -1,2 +1,27 @@
+#include "indexes/HAMTIndex.h"
+
+#include <string_view>
+
+#include <spdlog/spdlog.h>
+
+#include "indexes/HAMTIndexManager.h"
+
+using namespace db;
+
 int main() {
+    GraphView view;
+    HAMTManager man;
+    PropertyTypeID pid {1};
+
+    HAMTIndex<std::string_view, NodeID> index("my index", &man, pid);
+    index.init(view);
+
+    std::string_view key {"my string"};
+    NodeID val{101};
+
+    index.mutableInsert(key, val);
+
+    const NodeID* n = index.find("my string");
+
+    spdlog::info("{}", n->getValue());
 }
--- storage/indexes/HAMTIndex.cpp
+++ storage/indexes/HAMTIndex.cpp
@@ -1,4 +1,9 @@
 #include "HAMTIndex.h"
+
+#include <bit>
+
+#include "ArcManager.h"
+
 #include "indexes/HAMTIndexNode.h"
 #include "metadata/PropertyType.h"
 
@@ -6,6 +11,7 @@ using namespace db;
 
 template <typename K, typename V, typename Hash>
 void HAMTIndex<K, V, Hash>::init(GraphView view) {
+    _root = _man->newInner();
 }
 
 template <typename K, typename V, typename Hash>
@@ -37,16 +43,105 @@ bool HAMTIndex<K, V, Hash>::isNodeIndex() const {
 
 template <typename K, typename V, typename Hash>
 void HAMTIndex<K, V, Hash>::mutableInsert(const K& key, const V& value) {
+    HAMTIndexNode* node = _root.get();
+
     const size_t hashcode = _hasher(key);
     size_t mask = _initialChunkMask;
+    size_t hashChunk = hashcode & mask;
 
-    [[maybe_unused]] size_t hashChunk = hashcode & mask;
+    size_t depth = 0;
 
-    WeakArc<HAMTIndexNode> node = _root;
+    while (node) {
+        const bool isLeaf = node->getKind() == HAMTIndexNode::Kind::LEAF;
 
-    [[maybe_unused]] const auto tryGetChild = [](size_t hashChunk) -> WeakArc<HAMTIndexNode> {
-        return {};
-    };
+        if (isLeaf) {
+            // TODO: Handle leaf replacement (if non terminal)/collision resultion (if
+            // terminal)
+            break;
+        }
+
+        auto* inner = node->getAs<HAMTInnerNode>();
+
+        const HAMTInnerNode::ChildBitmask childMask = inner->mask();
+
+        const size_t bitIndex = 1UL << hashChunk;
+        const size_t chldrnBelowMask = bitIndex - 1;
+        const size_t chldrnLesser = childMask & chldrnBelowMask;
+        const size_t chldrnLesserCount = std::popcount(chldrnLesser);
+
+        const bool exists = (bitIndex & childMask) != 0;
+
+        if (exists) {
+            HAMTInnerNode::Children& chldrn = inner->_children;
+            node = chldrn[chldrnLesserCount].get();
+            depth++;
+            hashChunk = (hashcode >> depth * _hashChunkSize) & _initialChunkMask;
+            continue;
+        }
+
+        // Doesn't exist: add this child as a leaf
+
+        WeakArc<HAMTIndexNode> rc = _man->newLeaf<K, V>();
+
+        { // Store the value for the inserted entry
+            auto* leaf = rc->getAs<HAMTLeaf<K, V>>();
+            leaf->emplace_back(key, value);
+        }
+
+        { // Update parent's child array and child bitmask
+            const auto after = inner->_children.begin() + chldrnLesserCount;
+            inner->_children.insert(after, rc);
+            inner->_mask |= bitIndex;
+        }
+
+        return;
+    }
+}
+
+template <typename K, typename V, typename Hash>
+const V* HAMTIndex<K, V, Hash>::find(const K& key) {
+    HAMTIndexNode* node = _root.get();
+
+    const size_t hashcode = _hasher(key);
+    size_t mask = _initialChunkMask;
+    size_t hashChunk = hashcode & mask;
+
+    size_t depth = 0;
+    while (node) {
+        const bool isLeaf = node->getKind() == HAMTIndexNode::Kind::LEAF;
+
+        if (isLeaf) {
+            auto* leaf = node->getAs<HAMTLeaf<K, V>>();
+            auto& pairs = leaf->_values;
+            for (const auto& [k, v] : pairs) {
+                if (k == key) {
+                    return &v;
+                }
+            }
+            return nullptr;
+        }
+
+        auto* inner = node->getAs<HAMTInnerNode>();
+
+        const HAMTInnerNode::ChildBitmask childMask = inner->mask();
+        const size_t bitIndex = 1UL << hashChunk;
+
+        const bool exists = (bitIndex & childMask) != 0;
+        if (!exists) {
+            return nullptr;
+        }
+
+        const size_t chldrnBelowMask = bitIndex - 1;
+        const size_t chldrnLesser = childMask & chldrnBelowMask;
+        const size_t chldrnLesserCount = std::popcount(chldrnLesser);
+
+        HAMTInnerNode::Children& chldrn = inner->_children;
+        node = chldrn[chldrnLesserCount].get();
+        depth++;
+        hashChunk = (hashcode >> depth * _hashChunkSize) & _initialChunkMask;
+    }
+
+    return nullptr;
 }
 
 namespace db {
