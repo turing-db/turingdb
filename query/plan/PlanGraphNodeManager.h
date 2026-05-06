b46e09d8769b733f4484ba4680b681dce068c17a
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-05-06 18:52:00 +0200

    Add PlanGraphNodeManager, change constructor of only PlanGraphNode

---
+++ query/plan/PlanGraphNodeManager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+#include <span>
+#include <vector>
+
+namespace db {
+
+class PlanGraphNode;
+
+class PlanGraphNodeManager {
+public:
+    template <typename T, typename... Args>
+    T* create(Args&&... args) {
+        auto node = std::make_unique<T>(_nextID, std::forward<Args>(args)...);
+        _nextID++;
+        auto* nodePtr = node.get();
+        _nodes.emplace_back(std::move(node));
+
+        return nodePtr;
+    }
+
+    std::span<const std::unique_ptr<PlanGraphNode>> nodes() const { return _nodes; }
+
+private:
+    std::vector<std::unique_ptr<PlanGraphNode>> _nodes;
+
+    uint64_t _nextID {0};
+};
+
+}
