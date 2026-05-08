3a7ec5f79ca2198df2ae6d8ca2ec923cccc05179
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-05-08 15:24:17 +0200

    Dockerfile: start turingdb daemon in entrypoint

--- run_visualizer.sh
+++ run_visualizer.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+turingdb start -demon -p 6666 -start-timeout 30000
+
 serve -s "$TURINGDB_VIS_DIR" -l "tcp://127.0.0.1:$TURINGDB_VIS_PORT" >/tmp/turingdb-visualizer.log 2>&1 &
 
 exec "$@"
