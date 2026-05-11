467f1da0d0bca47ab2637b99ff9811f92b96face
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-05-11 18:49:17 +0200

    Add benchmark in sample

--- samples/hamt/main.cpp
+++ samples/hamt/main.cpp
@@ -1,13 +1,35 @@
+#include <chrono>
+#include <iostream>
 #include <string_view>
 
 #include <spdlog/spdlog.h>
 
 #include "indexes/HAMTIndex.h"
 #include "indexes/HAMTIndexManager.h"
+#include "metadata/PropertyType.h"
 
 using namespace db;
 
-int main() {
+static const auto now = []() -> auto {
+    return std::chrono::high_resolution_clock::now();
+};
+
+static constexpr auto printTime = [](std::string_view label, const auto& time) -> void {
+    const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(time).count();
+    std::cout << label << ": ";
+    if (ns < 1'000) {
+        std::cout << ns << "ns";
+    } else if (ns < 1'000'000) {
+        std::cout << ns / 1'000.0 << "us";
+    } else if (ns < 1'000'000'000) {
+        std::cout << ns / 1'000'000.0 << "ms";
+    } else {
+        std::cout << ns / 1'000'000'000.0 << "s";
+    }
+    std::cout << '\n';
+};
+
+void basictest() {
     GraphView view;
     HAMTManager man;
     PropertyTypeID pid {1};
@@ -50,4 +72,30 @@ int main() {
 
         spdlog::info("{}", n->getValue());
     }
+}
+
+void loadtest(size_t numPairs) {
+    GraphView view;
+    HAMTManager man;
+    PropertyTypeID pid {1};
+
+    HAMTIndex<types::UInt64::Primitive, NodeID> index("my index", &man, pid);
+    index.init(view);
+
+    {
+        const auto start = now();
+        for (size_t i = 0; i < numPairs; i++) {
+            const types::UInt64::Primitive key = i;
+            const NodeID value = i;
+
+            index.exhaustiveMutInsert(key, value);
+        }
+        const auto taken = now() - start;
+        printTime("build", taken);
+    }
+}
+
+int main() {
+    basictest();
+    loadtest(1'000'000);
 }
