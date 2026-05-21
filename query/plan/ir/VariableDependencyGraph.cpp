7e847fbbb0f82cf85a0c3806c98865fa89dfa5d8
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-05-21 15:49:42 +0200

    Working dependency dump

--- query/plan/ir/VariableDependencyGraph.cpp
+++ query/plan/ir/VariableDependencyGraph.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <ostream>
+#include <unordered_map>
 
 #include "EntityPattern.h"
 #include "PatternElement.h"
@@ -18,12 +19,15 @@ void VariableDependencyGraph::registerPatternElement(const PatternElement* ptn) 
 
     const auto& chain = ptn->getElementChain();
 
+    VariableDependency* prev = originVar;
     for (const auto& [edge, tgt] : chain) {
         VariableDependency* edgeVar = getOrCreateVariable(edge);
         VariableDependency* tgtVar = getOrCreateVariable(tgt);
 
-        originVar->addOutgoing(edgeVar);
-        originVar->addOutgoing(tgtVar);
+        prev->addOutgoing(edgeVar);
+        edgeVar->addOutgoing(tgtVar);
+
+        prev = tgtVar;
     }
 }
 
@@ -33,29 +37,29 @@ VariableDependency* VariableDependencyGraph::newVariable(const EntityPattern* en
 }
 
 void VariableDependencyGraph::dump(std::ostream& out) const {
-    out << "VariableDependencyGraph (" << _vars.size() << " nodes):\n";
+    out << "flowchart TD\n";
+
+    std::unordered_map<const VariableDependency*, size_t> nodeIds;
+    for (size_t i = 0; i < _vars.size(); i++) {
+        nodeIds[&_vars[i]] = i;
+    }
+
+    const auto nodeDef = [&](const VariableDependency* var) {
+        const std::string_view name = var->entity()->getDecl()->getName();
+        const std::string_view label = name.empty() ? "<unnamed>" : name;
+        return "v" + std::to_string(nodeIds.at(var)) + "[\"" + std::string(label) + "\"]";
+    };
 
     for (const VariableDependency& var : _vars) {
-        const std::string_view name = var.entity()->getDecl()->getName();
-        const bool unnamed = var.entity()->getDecl()->isUnnamed();
+        const bool isolated = var.isRoot() && var.getOutgoing().empty();
 
-        out << "  " << (unnamed ? "<unnamed>" : name);
-
-        if (var.isRoot()) {
-            out << " [root]";
-        }
-
-
-        if (!var.getOutgoing().empty()) {
-            out << " ->";
+        if (isolated) {
+            out << "    " << nodeDef(&var) << "\n";
+        } else {
             for (const VariableDependency* dep : var.getOutgoing()) {
-                const std::string_view depName = dep->entity()->getDecl()->getName();
-                const bool depUnnamed = dep->entity()->getDecl()->isUnnamed();
-                out << " " << (depUnnamed ? "<unnamed>" : depName);
+                out << "    " << nodeDef(&var) << " --> " << nodeDef(dep) << "\n";
             }
         }
-
-        out << "\n";
     }
 }
 
---
+++ samples/ir/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./ir "$@" | awk '/^flowchart/,0' | curl -s https://mermaid-ascii.art --data-urlencode 'mermaid@-'
