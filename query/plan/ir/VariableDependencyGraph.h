bbc7e0e934d282c51794d3a409f7528204598aa8
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-05-21 12:13:51 +0200

    Begin variable dep graph

--- query/plan/ir/VariableDependencyGraph.h
+++ query/plan/ir/VariableDependencyGraph.h
@@ -1,9 +1,45 @@
 #pragma once
 
+#include <deque>
+
+#include "EntityPattern.h"
+
+#include "SmallVector.h"
+
 namespace db {
 
+class VariableDependency;
+
 class VariableDependencyGraph {
-    
+public:
+    const VariableDependency* getOrCreateVariable(const EntityPattern* entity);
+
+private:
+    std::deque<VariableDependency> _vars;
+
+    const VariableDependency* newVariable(const EntityPattern* entity);
+};
+
+class VariableDependency {
+public:
+    using Deps = SmallVector<const VariableDependency*, 4>;
+
+    VariableDependency(const EntityPattern* entity)
+        : _entity(entity)
+    {
+    }
+
+    void addIncoming(const VariableDependency* dep) { _incoming.push_back(dep); }
+    void addOutgoing(const VariableDependency* dep) { _outgoing.push_back(dep); }
+
+    const EntityPattern* entity() const { return _entity; }
+
+    bool isRoot() const { return _incoming.empty(); }
+
+private:
+    const EntityPattern* _entity {nullptr};
+    Deps _incoming;
+    Deps _outgoing;
 };
 
 }
