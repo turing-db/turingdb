a1be430a7faf71bea9e7d350f85baec7e2499846
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-02 20:03:36 +0200

    Reshuffle directory and includes

---
+++ query/ir/dialect/db/DBOps.cpp
@@ -0,0 +1,5 @@
+#include "DBOps.h"
+#include "DBDialect.h"
+
+#define GET_OP_CLASSES
+#include "DBOps.cpp.inc"
--- samples/mlir/main.cpp
+++ samples/mlir/main.cpp
@@ -1,5 +1,25 @@
 #include <print>
 
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Builders.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "DBOps.h"
+
 int main() {
     std::println("hello from mlir");
+
+    mlir::MLIRContext ctx;
+    ctx.loadDialect<mlir::turing::TuringDB>();
+
+    mlir::OpBuilder builder(&ctx);
+    mlir::Location loc = builder.getUnknownLoc();
+    mlir::Type resultType = mlir::IntegerType::get(&ctx, 64);
+
+    auto op = builder.create<mlir::turing::ScanNodes>(loc, resultType);
+
+    op->print(llvm::outs());
+    llvm::outs() << "\n";
+
+    return 0;
 }
