d515348d53974ce24b298cc64d72eadde3dbeea4
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-02 20:03:36 +0200

    Reshuffle directory and includes

--- query/ir/dialect/db/DBDialect.cpp
+++ query/ir/dialect/db/DBDialect.cpp
@@ -1,1 +1,15 @@
+#include "DBDialect.h"
+#include "DBOps.h"
 
+using namespace mlir;
+using namespace mlir::turing;
+
+#include "DBDialect.cpp.inc"
+
+void TuringDB::initialize() {
+    addOperations<
+#define GET_OP_LIST
+#include "DBOps.cpp.inc"
+    >();
+    // registerTypes();
+}
--- query/ir/dialect/db/DBOps.h
+++ query/ir/dialect/db/DBOps.h
@@ -1,1 +1,11 @@
+#pragma once
 
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#include "DBDialect.h"
+
+#define GET_OP_CLASSES
+#include "DBOps.h.inc"
