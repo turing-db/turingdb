a1199189007a9864d900bdd39fd521f0ff814189
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-03 16:39:45 +0200

    Enable GetOutEdges, use in example

--- samples/mlir/main.cpp
+++ samples/mlir/main.cpp
@@ -3,25 +3,46 @@
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Builders.h"
 #include "llvm/Support/raw_ostream.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 
 #include "DBOps.h"
+
+static void print(auto op) {
+    op->print(llvm::outs());
+    llvm::outs() << '\n';
+}
+
 
 int main() {
     std::cout << "hello from mlir" << '\n';
 
     mlir::MLIRContext ctx;
     ctx.loadDialect<mlir::turing::TuringDB>();
+    ctx.loadDialect<mlir::func::FuncDialect>();
 
     mlir::OpBuilder builder(&ctx);
     mlir::Location loc = builder.getUnknownLoc();
-    mlir::Type resultCol = mlir::turing::ColumnType::get(&ctx, "nodes");
 
-    auto op = builder.create<mlir::turing::ScanNodes>(loc, resultCol);
+    auto module = mlir::ModuleOp::create(loc);
 
-    if (mlir::failed(op.verify())) {
-        llvm::errs() << "op verification failed\n";
-    }
+    auto funcType = mlir::FunctionType::get(&ctx, {}, {});
+    auto func = builder.create<mlir::func::FuncOp>(loc, "main", funcType);
+    auto& block = *func.addEntryBlock();
+    builder.setInsertionPointToStart(&block);
+    module.push_back(func);
 
-    op->print(llvm::outs());
-    llvm::outs() << "\n";
+    mlir::Type col0 = mlir::turing::ColumnType::get(&ctx, "scan");
+    mlir::Type col1 = mlir::turing::ColumnType::get(&ctx, "srcs");
+    mlir::Type col2 = mlir::turing::ColumnType::get(&ctx, "eids");
+    mlir::Type col3 = mlir::turing::ColumnType::get(&ctx, "etypes");
+    mlir::Type col4 = mlir::turing::ColumnType::get(&ctx, "tgts");
+
+    auto scan = builder.create<mlir::turing::ScanNodes>(loc, col0);
+    auto getout = builder.create<mlir::turing::GetOutEdges>(loc, col1, col2, col3, col4,
+                                                            scan.getResult());
+
+    print(scan);
+    print(getout);
 }
