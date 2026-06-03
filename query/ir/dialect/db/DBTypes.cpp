eb817577c20e8e826cb57ffa5fe5fdd2d84a4177
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-03 15:51:03 +0200

    Declare column type

--- query/ir/dialect/db/DBTypes.cpp
+++ query/ir/dialect/db/DBTypes.cpp
@@ -2,6 +2,10 @@
 
 #include "DBDialect.h"
 
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 using namespace mlir::turing;
 
 #define GET_TYPEDEF_CLASSES
