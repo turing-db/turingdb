f427b706dfa3c2867cde6f9edb39f24c2652f4b5
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-03 16:39:45 +0200

    Enable GetOutEdges, use in example

--- query/ir/dialect/db/DBOps.h
+++ query/ir/dialect/db/DBOps.h
@@ -4,6 +4,7 @@
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "DBDialect.h"
