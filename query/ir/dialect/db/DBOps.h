652c67856ab8b4cdefb9bac08704fcaf512291e2
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-03 11:03:30 +0200

    Add missing Builders include

--- query/ir/dialect/db/DBOps.h
+++ query/ir/dialect/db/DBOps.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
