3447138d8aeb944bec8dfef051628b27d08d6dba
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-04 13:44:01 +0200

    Add explicit edge type, with metadata

--- samples/vardeps/run.sh
+++ samples/vardeps/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./ir "$@" | awk '/^flowchart/,0' | curl -s https://mermaid-ascii.art --data-urlencode 'mermaid@-'
+./vardeps "$@" | awk '/^flowchart/,0' | curl -s https://mermaid-ascii.art --data-urlencode 'mermaid@-'
 echo ""
