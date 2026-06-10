4e9355bc96fa4368cef1d0629dd1dad11dacbfc6
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-10 14:07:35 +0200

    Working in test case

--- samples/jsonl-import/main.cpp
+++ samples/jsonl-import/main.cpp
@@ -1,12 +1,20 @@
+#include <cstdlib>
 #include <fstream>
 #include <memory>
+#include <string>
+
+#include <argparse.hpp>
+#include <spdlog/spdlog.h>
 
 #include "Graph.h"
 #include "JobSystem.h"
 #include "JsonlParser.h"
+#include "LocalMemory.h"
+#include "QueryCallbacks.h"
 #include "SystemManager.h"
 #include "TuringConfig.h"
 #include "TuringDB.h"
+#include "dataframe/Dataframe.h"
 #include "reader/GraphReader.h"
 #include "versioning/Change.h"
 #include "versioning/ChangeAccessor.h"
@@ -14,15 +22,34 @@
 
 using namespace db;
 
-int main(int argc, const char** argv) {
-    if (argc < 2) {
-        fmt::println("Usage: {} <jsonl-file>", argv[0]);
+int main(int argc, char** argv) {
+    argparse::ArgumentParser parser("jsonl-import", "1.0", argparse::default_arguments::help);
+    parser.add_description("Import a JSONL graph file into TuringDB");
+
+    std::string inputFile;
+    std::string query;
+
+    parser.add_argument("file", "f")
+        .metavar("graph.jsonl")
+        .store_into(inputFile)
+        .help("JSONL file to import");
+
+    parser.add_argument("--query", "q")
+        .metavar("QUERY")
+        .store_into(query)
+        .help("Cypher query to run after import");
+
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+        std::cerr << parser;
         return EXIT_FAILURE;
     }
 
-    std::ifstream file(argv[1]);
+    std::ifstream file(inputFile);
     if (!file.is_open()) {
-        fmt::println("Could not open file: {}", argv[1]);
+        fmt::println("Could not open file: {}", inputFile);
         return EXIT_FAILURE;
     }
 
@@ -44,6 +71,7 @@ int main(int argc, const char** argv) {
     config.setTuringDirectory(turingDir);
     TuringDB db(&config);
 
+    constexpr std::string_view graphName = "test";
     SystemManager& sysMan = db.getSystemManager();
     SystemAccessor system = sysMan.accessUnique();
     Graph* graph = system.createGraph("test");
@@ -73,6 +101,20 @@ int main(int argc, const char** argv) {
     fmt::println("Graph has {} nodes and {} edges", reader.getNodeCount(), reader.getEdgeCount());
 
     system.dumpGraph("test");
+    if (!query.empty()) {
+        LocalMemory mem;
+        QueryCallbacks cbs;
+        const auto dump = [](const Dataframe* d) { d->dump(std::cout); };
+        cbs.setOnOutputData(dump);
+        QueryState state(graphName, &mem, &db.getDefaultQueryConfig(), &cbs);
+        const auto res = db.query(query, state);
+        if (!res) {
+            spdlog::error(res.getError());
+            return EXIT_FAILURE;
+        }
+    }
+
+    sysMan.dumpGraph("test");
 
     return EXIT_SUCCESS;
 }
