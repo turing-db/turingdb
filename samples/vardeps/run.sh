536a4412b92e7a5c29248d9711ce009b052ab967
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-17 16:50:05 +0200

    Clean up sample, toplevel CMake

--- samples/vardeps/run.sh
+++ samples/vardeps/run.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# for CI runner, just exit success
-if ! command -v nix-shell &>/dev/null; then
-    exit 0
-fi
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"$SCRIPT_DIR/vardeps" "$@" \
-    | awk '/^flowchart/,0' \
-    | nix-shell "$SCRIPT_DIR/shell.nix" --run "python3 $SCRIPT_DIR/asciimermaid.py"
-echo ""
+./vardeps -q "MATCH (n) RETURN n"
