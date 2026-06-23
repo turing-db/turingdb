903ba11db425784de161fb5c8b0ae4f432e133a9
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-23 13:03:16 +0200

    Begin Parquet loader

---
+++ io/parquet/ParquetNeo4jDumpLoader.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <limits>
+#include <vector>
+
+#include <stddef.h>
+
+namespace db {
+
+class ParquetNeo4jDumpLoader {
+public:
+private:
+    friend class ParquetNeo4jVisitor;
+
+    static constexpr size_t INVALID_COL_IDX = std::numeric_limits<size_t>::max();
+
+    size_t _nodeColIdx {INVALID_COL_IDX};
+    size_t _lblColIdx {INVALID_COL_IDX};
+
+    size_t _srcColIdx {INVALID_COL_IDX};
+    size_t _tgtColIdx {INVALID_COL_IDX};
+    size_t _edgetypeColIdx {INVALID_COL_IDX};
+
+    std::vector<size_t> _propCols;
+};
+
+}
