6e34ed97d9016213c00a83612b3c93ae7dcfba6a
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-26 11:13:43 +0200

    Blueprint for reading labels

--- import/parquet/Neo4jParquetImporter.cpp
+++ import/parquet/Neo4jParquetImporter.cpp
@@ -1,10 +1,19 @@
 #include "Neo4jParquetImporter.h"
 
+#include <parquet/types.h>
+#include <range/v3/view/zip.hpp>
+
+#include "ParquetNeo4jVisitor.h"
 #include "ParquetReader.h"
+
+#include "versioning/CommitBuilder.h"
+#include "writers/DataPartBuilder.h"
 
 #include "BioAssert.h"
 
 using namespace db;
+namespace rg = ranges;
+namespace rv = rg::views;
 
 void Neo4jParquetImporter::import() {
     // Opens file; populates metadata; calls @ref ParquetNeo4jVisitor::onFileStart,
@@ -14,10 +23,22 @@ void Neo4jParquetImporter::import() {
     bioassert(_visitor._nodeColIdx != ParquetNeo4jVisitor::INVALID_COL_IDX,
               "failed to populate node column");
 
-    _reader.nextChunk();
+    while (_reader.nextChunk()) {
+        addNodes();
+    }
+}
 
-    const auto nodeIDs = _visitor.nodes();
-    const auto labels = _visitor.labels();
+void Neo4jParquetImporter::addNodes() {
+    const ParquetNeo4jVisitor::NodeIDs nodeIDs = _visitor.nodes();
+    const ParquetNeo4jVisitor::Labels labels = _visitor.labels();
 
-    bioassert(nodeIDs.size() == labels.size(), "Mismatched labels and nodes ({}, {})", nodeIDs.size(), labels.size());
+    bioassert(nodeIDs.data(), "Failed to get nodes");
+    bioassert(labels.data(), "Failed to get nodes");
+    bioassert(nodeIDs.size() == labels.size(), "Node, label mismatch");
+
+    DataPartBuilder& bld = _builder->getCurrentBuilder();
+
+    std::vector<std::string> labelBuffer;
+    for (auto [id, lbls] : rv::zip(nodeIDs, labels)) {
+    }
 }
