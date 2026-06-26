bf2c778b4d025f3364d9dbf6f11853519b949db9
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-26 11:13:43 +0200

    Blueprint for reading labels

--- import/parquet/Neo4jParquetImporter.h
+++ import/parquet/Neo4jParquetImporter.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <stddef.h>
+#include <unordered_map>
 
+#include "ID.h"
 #include "ParquetNeo4jVisitor.h"
 
 #include "Path.h"
@@ -17,7 +19,8 @@ public:
     explicit Neo4jParquetImporter(fs::Path path, CommitBuilder* builder)
         : _path(std::move(path)),
         _visitor(builder),
-        _reader(_path, _visitor)
+        _reader(_path, _visitor),
+        _builder(builder)
     {
     }
 
@@ -26,9 +29,16 @@ public:
     void import();
 
 private:
+    // Maps Neo4j IDs to TuringDB IDs
+    using IDMap = std::unordered_map<int64_t, NodeID>;
+
     fs::Path _path;
     ParquetNeo4jVisitor _visitor;
     ParquetReader _reader;
+
+    CommitBuilder* _builder {nullptr};
+
+    void addNodes();
 };
 
 }
