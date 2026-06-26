3494dabdea53eda41eacaee05068e159bab90b6d
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-26 15:23:37 +0200

    New db generator

---
+++ query/ir/codegen/DBProgramGenerator.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+#include "mlir/IR/Value.h"
+
+#include "DBTypes.h"
+
+namespace mlir {
+class ModuleOp;
+class OpBuilder;
+}
+
+namespace db {
+
+class CypherAST;
+class VariableDependency;
+
+class DBProgramGenerator {
+public:
+    using VariableIdentities = std::vector<mlir::TypedValue<mlir::db::ColumnType>>;
+    using VariableIdentityMap = std::unordered_map<const VariableDependency*, VariableIdentities>;
+
+    void generate(const CypherAST* ast, mlir::ModuleOp* module);
+private:
+    mlir::MLIRContext* _mlirCtxt {nullptr};
+    mlir::OpBuilder* _opBuilder {nullptr};
+
+    VariableIdentityMap _varMap;
+
+    void addScanNodes(const VariableDependency* var);
+    void addGetOutEdges(const VariableDependency* src, const VariableDependency* edge, const VariableDependency* tgt);
+
+    mlir::db::ColumnType allocColumnType(const VariableDependency* var);
+    void registerValue(const VariableDependency* var, mlir::TypedValue<mlir::db::ColumnType> val);
+};
+
+}
