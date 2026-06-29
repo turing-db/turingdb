19d4fc434eae36532b9d4dabcdd3f1f6c2f73a9b
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-06-29 16:02:25 +0200

    Constant takes TypedAttr

--- query/ir/dialect/db/DBOps.h
+++ query/ir/dialect/db/DBOps.h
@@ -7,6 +7,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 
 #include "DBDialect.h"
 #include "DBTypes.h"
