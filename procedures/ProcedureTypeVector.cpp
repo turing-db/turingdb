a7c4b4388676490941a37f98e1ff81268e0143c5
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-07-31 15:58:01 +0200

    Keep count of required args

--- procedures/ProcedureTypeVector.cpp
+++ procedures/ProcedureTypeVector.cpp
@@ -9,6 +9,9 @@ ProcedureTypeVector::ProcedureTypeVector()
 ProcedureTypeVector::ProcedureTypeVector(std::initializer_list<NamedProcedureType> values)
     : _values(values)
 {
+    for (const NamedProcedureType& arg : _values) {
+        _requiredCount += !arg._optional;
+    }
 }
 
 ProcedureTypeVector::~ProcedureTypeVector() {
