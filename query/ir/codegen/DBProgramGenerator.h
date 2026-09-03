81cdf40b6f9d8370ec5e442659bea1497c9d88ca
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-09-03 18:45:04 +0200

    Analyse shortest path, test, clear drains

--- query/ir/codegen/DBProgramGenerator.h
+++ query/ir/codegen/DBProgramGenerator.h
@@ -483,8 +483,6 @@ private:
 
     mlir::Value resolveEntityColumn(const VarDecl* decl);
 
-    const VarDecl* declForVariableName(std::string_view name) const;
-
     mlir::Value nullConstantColumn();
 
     // Taken in the order the query declares its variables, so the choice is the query's
