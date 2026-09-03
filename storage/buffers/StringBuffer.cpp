0139d096843374ac0379935ec666f40576e0e816
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-09-03 18:27:07 +0200

    Add StringBuffer::join

--- storage/buffers/StringBuffer.cpp
+++ storage/buffers/StringBuffer.cpp
@@ -1,8 +1,16 @@
 #include "StringBuffer.h"
 
 #include <string.h>
+#include <string_view>
+#include <functional>
+
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/drop.hpp>
 
 using namespace db;
+
+namespace rg = ranges;
+namespace rv = rg::views;
 
 std::string_view StringBuffer::concatenate(std::string_view a, std::string_view b) {
     const size_t totalSize = a.size() + b.size();
@@ -17,4 +25,42 @@ std::string_view StringBuffer::concatenate(std::string_view a, std::string_view 
     _buf.commit(totalSize);
 
     return {start, totalSize};
+}
+
+std::string_view StringBuffer::join(std::span<const std::string_view> strs, std::string_view sep) {
+    const size_t numElements = strs.size();
+
+    if (numElements == 0) {
+        return {_buf.nextPtr(), 0};
+    }
+
+    const size_t totalChars = [&] -> size_t {
+        const size_t sepSize = sep.size();
+        const size_t numSeps = numElements - 1;
+        const size_t eleLen = rg::accumulate(strs, 0UZ, std::plus<> {}, rg::size);
+
+        return eleLen + (sepSize * numSeps);
+    }();
+
+    _buf.reserveContiguous(totalChars);
+
+    char* ptr = _buf.nextPtr();
+    const char* start = ptr;
+
+    const auto write = [&](std::string_view str) {
+        memcpy(ptr, str.data(), str.size());
+        ptr += str.size();
+    };
+
+    const std::string_view fst = strs.front();
+    write(fst);
+
+    for (const std::string_view next : strs | rv::drop(1)) {
+        write(sep);
+        write(next);
+    }
+
+    _buf.commit(totalChars);
+
+    return {start, totalChars};
 }
--- storage/buffers/StringBuffer.h
+++ storage/buffers/StringBuffer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <span>
 #include <string_view>
 
 #include "SpanBuffer.h"
@@ -9,6 +10,8 @@ namespace db {
 class StringBuffer final : public SpanBuffer<char, std::string_view> {
 public:
     std::string_view concatenate(std::string_view a, std::string_view b);
+
+    std::string_view join(std::span<const std::string_view> strs, std::string_view sep);
 };
 
 }
