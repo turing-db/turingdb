9002f4814d45100da35df7a01b6db367f3466d4c
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-09-10 18:39:51 +0200

    Ensure PropertyContainer<String> returns end on null index

--- storage/properties/PropertyContainer.h
+++ storage/properties/PropertyContainer.h
@@ -276,6 +276,10 @@ public:
 
         const size_t offset = it->second;
 
+        if (offset == NULL_INDEX) {
+            return _values.end();
+        }
+
         return _values.begin() + offset;
     }
 
