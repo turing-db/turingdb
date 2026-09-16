fc699cf084fe21e6447e48a111c05e847f97f301
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-09-16 17:00:05 +0200

    Begin sampler

--- procedures/GraphSAGEProcedure.cpp
+++ procedures/GraphSAGEProcedure.cpp
@@ -1,16 +1,20 @@
 #include "GraphSAGEProcedure.h"
 
-#include "FatalException.h"
 #include "Procedure.h"
 #include "ProcedureData.h"
 #include "ProcedureNamespace.h"
 #include "ProcedureState.h"
+
+#include "FatalException.h"
 
 using namespace db;
 
 namespace {
 
 void prepareImpl(ProcedureState* state) {
+}
+
+void executeImpl(ProcedureState* state) {
 }
 
 struct Data final : public ProcedureData {
@@ -36,9 +40,10 @@ void GraphSAGEProcedure::registerProcedure(ProcedureNamespace* ns) {
     ns->addProcedure(proc);
 }
 
-void GraphSAGEProcedure::execute(ProcedureState* proc) {
-    switch (proc->getStep()) {
+void GraphSAGEProcedure::execute(ProcedureState* state) {
+    switch (state->getStep()) {
         case ProcedureState::Step::PREPARE:
+            prepareImpl(state);
             throw FatalException("prepare not implemented");
         break;
 
@@ -47,6 +52,7 @@ void GraphSAGEProcedure::execute(ProcedureState* proc) {
         break;
 
         case ProcedureState::Step::EXECUTE:
+            executeImpl(state);
             throw FatalException("execute not implemented");
         break;
     }
--- procedures/GraphSAGEProcedure.h
+++ procedures/GraphSAGEProcedure.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "samplers/GraphSAGESampler.h"
+
+#include <stddef.h>
+
 namespace db {
 
 class ProcedureData;
@@ -11,6 +15,8 @@ struct GraphSAGEProcedure {
     static void deallocData(ProcedureData* data);
     static void execute(ProcedureState* proc);
     static void registerProcedure(ProcedureNamespace* ns);
+
+    static constexpr size_t numHops = GraphSAGESampler::hops;
 };
 
 }
