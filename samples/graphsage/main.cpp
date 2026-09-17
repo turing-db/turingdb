bfa0ac25537eee02e81e4214210bd27cfccbf634
Cyrus Knopf <cyrus.knopf@gmail.com>
2026-09-17 14:38:28 +0200

    Sample improvements, update reqlen on seeds

--- samples/graphsage/main.cpp
+++ samples/graphsage/main.cpp
@@ -2,8 +2,12 @@
 #include <algorithm>
 #include <array>
 #include <charconv>
+#include <iostream>
 #include <memory>
 #include <optional>
+#include <ostream>
+#include <span>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -110,14 +114,70 @@ void printEdgeColumns(const NodeCol& srcs, const NodeCol& tgts, const NodeNames&
     }
 }
 
+struct DumpColumn {
+    std::string _name;
+    const NodeCol* _column {nullptr};
+};
+
+void dumpColumns(std::ostream& out, std::span<const DumpColumn> columns) {
+    if (columns.empty()) {
+        return;
+    }
+
+    std::vector<std::ostringstream> columnStreams(columns.size());
+    for (size_t column = 0; column < columns.size(); column++) {
+        columns[column]._column->dump(columnStreams[column]);
+    }
+
+    std::vector<std::vector<std::string>> columnLines;
+    columnLines.reserve(columns.size());
+    size_t maxLines = 0;
+
+    for (std::ostringstream& stream : columnStreams) {
+        std::vector<std::string>& lines = columnLines.emplace_back();
+        std::istringstream in(stream.str());
+
+        std::string line;
+        while (std::getline(in, line)) {
+            lines.push_back(line);
+        }
+
+        maxLines = std::max(maxLines, lines.size());
+    }
+
+    for (const DumpColumn& column : columns) {
+        out << "Column [" << column._name << "]\t";
+    }
+    out << '\n';
+
+    for (size_t row = 0; row < maxLines; row++) {
+        for (size_t column = 0; column < columns.size(); column++) {
+            const std::vector<std::string>& lines = columnLines[column];
+
+            if (row < lines.size()) {
+                out << lines[row];
+            } else {
+                out << "_";
+            }
+
+            out << "\t";
+        }
+
+        out << '\n';
+    }
+}
+
 }
 
 int main(int argc, const char** argv) {
     ToolInit toolInit("graphsage");
     toolInit.disableOutputDir();
 
+    // args
     std::string seedList = "0";
     std::string fanoutList = "2,2,2";
+    bool explain = false;
+    bool dump = false;
 
     auto& argParser = toolInit.getArgParser();
     argParser.add_argument("--seeds", "-s")
@@ -128,6 +188,15 @@ int main(int argc, const char** argv) {
              .metavar("n,n,n")
              .store_into(fanoutList)
              .help(fmt::format("Comma separated neighbourhood sample size per hop (default: {})", fanoutList));
+    argParser.add_argument("--explain-", "-e")
+        .store_into(explain)
+        .flag()
+        .help(fmt::format("Print hop-by-hop explanation", explain));
+    argParser.add_argument("--dump-", "-d")
+        .store_into(dump)
+        .flag()
+        .help(fmt::format("Dump dataframe", dump));
+
 
     toolInit.init(argc, argv);
 
@@ -179,11 +248,11 @@ int main(int argc, const char** argv) {
     NodeNames names;
     collectNodeNames(reader, names);
 
-    std::array<NodeCol, hops> srcs;
-    std::array<NodeCol, hops> tgts;
-    std::array<NodeCol, hops> dstNodes;
+    std::array<NodeCol, hops> srcs {};
+    std::array<NodeCol, hops> tgts {};
+    std::array<NodeCol, hops> dstNodes {};
 
-    GraphSAGESampler sampler(&view);
+    GraphSAGESampler sampler(view);
     for (size_t hop = 0; hop < hops; hop++) {
         sampler.setHopData(hop, &srcs[hop], &tgts[hop], &dstNodes[hop], fanouts[hop]);
     }
@@ -192,10 +261,26 @@ int main(int argc, const char** argv) {
 
     fmt::print("\nseeds: {}\nfanouts: {}\n", seedList, fanoutList);
 
-    for (size_t hop = 0; hop < hops; hop++) {
-        fmt::print("\n=== hop {} (fanout {}) ===\n", hop, fanouts[hop]);
-        printNodeColumn("dstNodes", dstNodes[hop], names);
-        printEdgeColumns(srcs[hop], tgts[hop], names);
+    if (explain) {
+        for (size_t hop = 0; hop < hops; hop++) {
+            fmt::print("\n=== hop {} (fanout {}) ===\n", hop, fanouts[hop]);
+            printNodeColumn("dstNodes", dstNodes[hop], names);
+            printEdgeColumns(srcs[hop], tgts[hop], names);
+        }
+    }
+
+    if (dump) {
+        std::vector<DumpColumn> columns;
+        columns.reserve(hops * 3);
+
+        for (size_t hop = 0; hop < hops; hop++) {
+            columns.emplace_back(fmt::format("dstNodes{}", hop), &dstNodes[hop]);
+            columns.emplace_back(fmt::format("srcs{}", hop), &srcs[hop]);
+            columns.emplace_back(fmt::format("tgts{}", hop), &tgts[hop]);
+        }
+
+        fmt::print("\n");
+        dumpColumns(std::cout, columns);
     }
 
     fmt::print("\n");
