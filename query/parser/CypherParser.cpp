cc0fe9571c62940c1f036d1f466b54ff6961e422
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-09-25 00:36:23 +0200

    Cypher: reject expressions nested deeper than 256 levels

--- query/parser/CypherParser.cpp
+++ query/parser/CypherParser.cpp
@@ -1,8 +1,14 @@
 #include "CypherParser.h"
 
 #include <sstream>
+#include <stdint.h>
+
+#include <spdlog/fmt/bundled/format.h>
 
 #include "CypherAST.h"
+#include "SourceLocation.h"
+#include "SourceManager.h"
+#include "expr/ExprDepth.h"
 #include "YCypherScanner.h"
 
 using namespace db;
@@ -24,4 +30,13 @@ void CypherParser::parse(std::string_view query) {
 
     yscanner.switch_streams(&iss, nullptr);
     yparser.parse();
+
+    const Expr* tooDeep = ExprDepth::findDeeperThan(_ast->getExpressions(), MAX_EXPRESSION_DEPTH);
+    if (tooDeep) {
+        const SourceManager* sourceManager = _ast->getSourceManager();
+        const SourceLocation* location = sourceManager->getLocation(reinterpret_cast<uintptr_t>(tooDeep));
+
+        yscanner.syntaxError(location ? *location : SourceLocation {},
+                             fmt::format("Expression nested deeper than {} levels", MAX_EXPRESSION_DEPTH));
+    }
 }
