e1637f0574a5f67418c9f735f2aa59497d0bca0c
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-09-25 02:57:23 +0200

    QA: pin the clause the error names for a REMOVE outside a change

---
+++ test/query/ir/RemoveOutsideChangeTest.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "QueryStatus.h"
+
+#include "WriteQueryTest.h"
+
+using namespace db;
+using namespace turing::test;
+
+class RemoveOutsideChangeTest : public WriteQueryTest {
+};
+
+TEST_F(RemoveOutsideChangeTest, namesRemoveInTheErrorForARemoveOutsideAChange) {
+    RowSink sink;
+    const QueryStatus status = runQuery("MATCH (p:Person {name: 'Remy'}) REMOVE p.age", &sink);
+    ASSERT_FALSE(status.isOk());
+
+    const std::string& error = status.getError();
+
+    EXPECT_NE(error.find("Cannot perform REMOVE outside of a write transaction."), std::string::npos)
+        << "error: " << error;
+}
+
+int main(int argc, char** argv) {
+    return turing::test::turingTestMain(argc, argv);
+}
