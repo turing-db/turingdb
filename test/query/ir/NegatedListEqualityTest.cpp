b79bf7439fe7d85a79e585a0b892cffc06473a8b
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-09-25 18:12:14 +0200

    QA: pin NOT over a constant list comparison with null

---
+++ test/query/ir/NegatedListEqualityTest.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "QueryInterpreterV3.h"
+#include "QueryStatus.h"
+
+#include "Graph.h"
+#include "SimpleGraph.h"
+#include "SystemAccessor.h"
+#include "SystemManager.h"
+#include "versioning/ChangeID.h"
+#include "versioning/CommitHash.h"
+
+#include "StringRowSink.h"
+#include "TuringTest.h"
+#include "TuringTestEnv.h"
+
+using namespace db;
+using namespace turing::test;
+
+class NegatedListEqualityTest : public TuringTest {
+protected:
+    void initialize() override {
+        _env = TuringTestEnv::create(fs::Path {_outDir} / "turing");
+        _interpreter = std::make_unique<QueryInterpreterV3>(&_env->getSystemManager());
+
+        SystemAccessor system = _env->getSystemManager().accessUnique();
+        Graph* graph = system.createGraph(_graphName);
+        SimpleGraph::createSimpleGraph(graph);
+    }
+
+    void runQuery(std::string_view query, StringRowSink& sink, QueryStatus& status) {
+        _interpreter->execute(status, query, _graphName, CommitHash::head(), ChangeID::head(), &_env->getMem(), &sink);
+    }
+
+    void expectRows(std::string_view query, const std::vector<StringRowSink::Row>& expected) {
+        StringRowSink sink;
+        QueryStatus status;
+        runQuery(query, sink, status);
+        ASSERT_TRUE(status.isOk()) << query << ": " << status.getError();
+
+        std::vector<StringRowSink::Row> actual;
+        sink.sortedRows(actual);
+
+        std::vector<StringRowSink::Row> sortedExpected = expected;
+        std::sort(sortedExpected.begin(), sortedExpected.end());
+
+        EXPECT_EQ(actual, sortedExpected) << "query: " << query;
+    }
+
+    const std::string _graphName = "simpledb";
+    std::unique_ptr<TuringTestEnv> _env;
+    std::unique_ptr<QueryInterpreterV3> _interpreter;
+};
+
+TEST_F(NegatedListEqualityTest, negatesAValueAgainstNullToNull) {
+    expectRows("UNWIND [1] AS x WITH x WHERE NOT ([1] = [null]) RETURN count(x)", {{"0"}});
+}
+
+TEST_F(NegatedListEqualityTest, negatesNullAgainstNullToNull) {
+    expectRows("UNWIND [1] AS x WITH x WHERE NOT ([null] = [null]) RETURN count(x)", {{"0"}});
+}
+
+TEST_F(NegatedListEqualityTest, negatesANullBesideAnEqualElementToNull) {
+    expectRows("UNWIND [1] AS x WITH x WHERE NOT ([1, 2] = [null, 2]) RETURN count(x)", {{"0"}});
+}
+
+int main(int argc, char** argv) {
+    return turing::test::turingTestMain(argc, argv);
+}
